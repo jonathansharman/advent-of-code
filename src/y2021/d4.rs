c18308543ffd7ba3fb370983e03967d13f114f18
Jonathan Sharman <jonathan.sharman@gmail.com>
2021-12-04 22:39:50 -0600

    Clean up day 4

--- src/y2021/d4.rs
+++ src/y2021/d4.rs
@@ -2,120 +2,104 @@ use crate::io::read_lines;
 
 use itertools::Itertools;
 
-use std::collections::HashSet;
+const SIDE_LENGTH: usize = 5;
 
-pub fn part1() -> i64 {
-	let mut lines = read_lines("input/2021/4-1.txt");
-	let numbers = lines
+struct Board {
+	spaces: [Option<i64>; SIDE_LENGTH * SIDE_LENGTH],
+	score: Option<i64>,
+}
+
+impl Board {
+	fn row(&self, idx: usize) -> impl Iterator<Item = &Option<i64>> {
+		self.spaces.iter().skip(idx * SIDE_LENGTH).take(SIDE_LENGTH)
+	}
+
+	fn col(&self, idx: usize) -> impl Iterator<Item = &Option<i64>> {
+		self.spaces.iter().skip(idx).step_by(SIDE_LENGTH)
+	}
+
+	fn tick(&mut self, n: i64) {
+		for idx in 0..self.spaces.len() {
+			if self.spaces[idx] == Some(n) {
+				self.spaces[idx] = None;
+				let (row_idx, col_idx) = (idx / SIDE_LENGTH, idx % SIDE_LENGTH);
+				if self.row(row_idx).all(Option::is_none) || self.col(col_idx).all(Option::is_none)
+				{
+					self.score = Some(
+						n * self
+							.spaces
+							.iter()
+							.fold(0, |acc, space| acc + space.unwrap_or(0)),
+					);
+				}
+				return;
+			}
+		}
+	}
+}
+
+fn read_numbers(lines: &mut impl Iterator<Item = String>) -> Vec<i64> {
+	lines
 		.next()
 		.expect("missing first line")
 		.split(',')
 		.map(|s| s.parse::<i64>().expect("failed to parse number"))
-		.collect::<Vec<i64>>();
+		.collect()
+}
 
-	let mut boards = Vec::<Board>::new();
-	for chunk in lines.chunks(6).into_iter() {
-		let mut board = Board { v: Vec::new() };
-		for line in chunk.skip(1) {
-			board.v.push(
-				line.split_whitespace()
-					.map(|s| Some(s.parse::<i64>().expect("failed to parse board space")))
-					.collect(),
-			);
-		}
-		boards.push(board);
-	}
+fn read_boards(lines: &mut impl Iterator<Item = String>) -> Vec<Board> {
+	lines
+		.chunks(SIDE_LENGTH + 1)
+		.into_iter()
+		.map(|chunk| Board {
+			spaces: chunk
+				.skip(1)
+				.flat_map(|line| {
+					line.split_whitespace()
+						.map(|s| Some(s.parse::<i64>().expect("failed to parse board space")))
+						.collect::<Vec<Option<i64>>>()
+				})
+				.collect::<Vec<Option<i64>>>()
+				.try_into()
+				.expect("wrong number of board spaces"),
+			score: None,
+		})
+		.collect()
+}
+
+pub fn part1() -> i64 {
+	let mut lines = read_lines("input/2021/4-1.txt");
+	let numbers = read_numbers(&mut lines);
+	let mut boards = read_boards(&mut lines);
 
 	for n in numbers {
 		for board in boards.iter_mut() {
-			board.check(n);
-			if board.bingo() {
-				return board.score() * n;
+			board.tick(n);
+			if let Some(score) = board.score {
+				return score;
 			}
 		}
 	}
 	0
-}
-
-struct Board {
-	v: Vec<Vec<Option<i64>>>,
-}
-
-impl Board {
-	fn check(&mut self, n: i64) {
-		for i in 0..5 {
-			for j in 0..5 {
-				if self.v[i][j] == Some(n) {
-					self.v[i][j] = None;
-				}
-			}
-		}
-	}
-
-	fn bingo(&self) -> bool {
-		'iloop: for i in 0..5 {
-			for j in 0..5 {
-				if self.v[i][j].is_some() {
-					continue 'iloop;
-				}
-			}
-			return true;
-		}
-		'jloop: for j in 0..5 {
-			for i in 0..5 {
-				if self.v[i][j].is_some() {
-					continue 'jloop;
-				}
-			}
-			return true;
-		}
-		false
-	}
-
-	fn score(&self) -> i64 {
-		self.v.iter().fold(0, |acc, row| {
-			acc + row.iter().fold(0, |acc, col| acc + col.unwrap_or(0))
-		})
-	}
 }
 
 pub fn part2() -> i64 {
 	let mut lines = read_lines("input/2021/4-2.txt");
-	let numbers = lines
-		.next()
-		.expect("missing first line")
-		.split(',')
-		.map(|s| s.parse::<i64>().expect("failed to parse number"))
-		.collect::<Vec<i64>>();
+	let numbers = read_numbers(&mut lines);
+	let mut boards = read_boards(&mut lines);
 
-	let mut boards = Vec::<Board>::new();
-	for chunk in lines.chunks(6).into_iter() {
-		let mut board = Board { v: Vec::new() };
-		for line in chunk.skip(1) {
-			board.v.push(
-				line.split_whitespace()
-					.map(|s| Some(s.parse::<i64>().expect("failed to parse board space")))
-					.collect(),
-			);
-		}
-		boards.push(board);
-	}
-	let boards_len = boards.len();
-
-	let mut winning_boards = HashSet::<usize>::new();
+	let mut last_score = 0;
 	for n in numbers {
-		for (idx, board) in boards.iter_mut().enumerate() {
-			if winning_boards.contains(&idx) {
-				continue;
-			}
-			board.check(n);
-			if board.bingo() {
-				winning_boards.insert(idx);
-				if winning_boards.len() == boards_len {
-					return board.score() * n;
-				}
+		for board in boards.iter_mut() {
+			board.tick(n);
+			if let Some(score) = board.score {
+				last_score = score;
 			}
 		}
+		boards.retain(|board| board.score.is_none());
+		if boards.is_empty() {
+			break;
+		}
 	}
-	0
+	last_score
 }
