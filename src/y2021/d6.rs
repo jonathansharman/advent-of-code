78038e3a88cfcd7eaeabd5fb47ae93daaae8ee71
Jonathan Sharman <jonathan.sharman@gmail.com>
2021-12-06 00:30:30 -0600

    Day 6 cleanup

--- src/y2021/d6.rs
+++ src/y2021/d6.rs
@@ -1,53 +1,38 @@
-use crate::io::read_lines;
-
-use std::collections::HashMap;
+use std::collections::{hash_map::Entry, HashMap};
 
 pub fn part1() -> i64 {
-	let mut fishes = read_lines("input/2021/6.txt")
-		.next()
-		.unwrap()
-		.split(',')
-		.map(&str::parse)
-		.map(Result::unwrap)
-		.collect::<Vec<i64>>();
-	for _ in 0..80 {
-		let mut new_fish = Vec::new();
-		for fish in fishes.iter_mut() {
-			*fish -= 1;
-			if *fish == -1 {
-				new_fish.push(8);
-				*fish = 6;
-			}
-		}
-		fishes.append(&mut new_fish);
-	}
-	fishes.len() as i64
+	fish(80)
 }
 
 pub fn part2() -> i64 {
-	let mut cache: HashMap<(i64, i64), i64> = HashMap::new();
-
-	read_lines("input/2021/6.txt")
-		.next()
-		.unwrap()
-		.split(',')
-		.map(|fish| fish.parse::<i64>().unwrap())
-		.fold(0, |acc, timer| acc + get(&mut cache, timer, 256))
+	fish(256)
 }
 
-fn get(cache: &mut HashMap<(i64, i64), i64>, timer: i64, days: i64) -> i64 {
-	if cache.contains_key(&(timer, days)) {
-		return cache[&(timer, days)];
+pub fn fish(days: i64) -> i64 {
+	let mut cache: HashMap<(i64, i64), i64> = HashMap::new();
+
+	std::fs::read_to_string("input/2021/6.txt")
+		.expect("error reading input")
+		.split(',')
+		.map(|fish| fish.parse::<i64>().expect("error parsing number"))
+		.fold(0, |acc, timer| acc + get_count(&mut cache, timer, days))
+}
+
+fn get_count(cache: &mut HashMap<(i64, i64), i64>, timer: i64, days: i64) -> i64 {
+	if days == 0 {
+		return 1;
 	}
-	let v = if days == 0 {
-		1
-	} else if timer == 0 {
-		get(cache, 6, days - 1) + get(cache, 8, days - 1)
+	if let Entry::Occupied(kv) = cache.entry((timer, days)) {
+		return *kv.get();
+	}
+
+	let count = if timer > 0 {
+		get_count(cache, timer - 1, days - 1)
 	} else {
-		get(cache, timer - 1, days - 1)
+		get_count(cache, 6, days - 1) + get_count(cache, 8, days - 1)
 	};
-	cache.insert((timer, days), v);
-	v
+	cache.insert((timer, days), count);
+	count
 }
 
 #[cfg(test)]
