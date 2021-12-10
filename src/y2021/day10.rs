b0692f943129195da464ac4818ccde8807ec4051
Jonathan Sharman <jonathan.sharman@gmail.com>
2021-12-10 00:16:47 -0600

    Day 10, part 2

--- src/y2021/day10.rs
+++ src/y2021/day10.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 use crate::{io::read_lines, solution::Solution};
 
 pub struct Day10;
@@ -13,16 +15,21 @@ impl Solution for Day10 {
 
 	fn part1(&self) -> i64 {
 		read_lines("input/2021/10.txt")
-			.map(|line| score(&line))
+			.map(|line| score1(&line))
 			.sum()
 	}
 
 	fn part2(&self) -> i64 {
-		0
+		let scores: Vec<i64> = read_lines("input/2021/10.txt")
+			.map(|line| score2(&line))
+			.filter(|&score| score != 0)
+			.sorted()
+			.collect();
+		scores[scores.len() / 2]
 	}
 }
 
-fn score(input: &str) -> i64 {
+fn score1(input: &str) -> i64 {
 	let mut stack = Vec::new();
 	for c in input.chars() {
 		match c {
@@ -47,6 +54,42 @@ fn score(input: &str) -> i64 {
 		}
 	}
 	0
+}
+
+fn score2(input: &str) -> i64 {
+	let mut stack = Vec::new();
+	for c in input.chars() {
+		match c {
+			'(' | '[' | '{' | '<' => stack.push(c),
+			')' => match stack.pop() {
+				Some('(') => (),
+				_ => return 0,
+			},
+			']' => match stack.pop() {
+				Some('[') => (),
+				_ => return 0,
+			},
+			'}' => match stack.pop() {
+				Some('{') => (),
+				_ => return 0,
+			},
+			'>' => match stack.pop() {
+				Some('<') => (),
+				_ => return 0,
+			},
+			_ => return 0,
+		}
+	}
+	stack.iter().rev().fold(0, |acc, c| {
+		5 * acc
+			+ match c {
+				'(' => 1,
+				'[' => 2,
+				'{' => 3,
+				'<' => 4,
+				_ => panic!("I didn't put that there"),
+			}
+	})
 }
 
 #[cfg(test)]
