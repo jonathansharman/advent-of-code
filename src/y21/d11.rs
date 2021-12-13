b434622c2aff71c7f5ca8d2ef053920b06acc376
Jonathan Sharman <jonathan.sharman@gmail.com>
2021-12-13 10:27:09 -0600

    Slight day 11 cleanup

--- src/y21/d11.rs
+++ src/y21/d11.rs
@@ -3,6 +3,8 @@ use crate::io::read_lines;
 crate::test::test_part!(test1, part1, 1757);
 crate::test::test_part!(test2, part2, 422);
 
+const WIDTH: usize = 10;
+
 pub fn part1() -> usize {
 	let mut grid: Vec<Vec<u32>> = read_lines("input/2021/11.txt")
 		.map(|line| line.chars().map(|c| c.to_digit(10).unwrap()).collect())
@@ -10,41 +12,44 @@ pub fn part1() -> usize {
 	let mut flashes = 0;
 	let mut queue = Vec::new();
 	for _ in 0..100 {
-		for i in 0..grid.len() {
-			for j in 0..grid[i].len() {
+		for i in 0..WIDTH {
+			for j in 0..WIDTH {
 				process(&mut grid, &mut queue, &mut flashes, i, j);
 			}
 		}
 		while !queue.is_empty() {
 			let (i, j) = queue.pop().unwrap();
+			let mut process = |i: usize, j: usize| {
+				process(&mut grid, &mut queue, &mut flashes, i, j);
+			};
 			if i > 0 {
-				process(&mut grid, &mut queue, &mut flashes, i - 1, j);
+				process(i - 1, j);
 				if j > 0 {
-					process(&mut grid, &mut queue, &mut flashes, i - 1, j - 1);
+					process(i - 1, j - 1);
 				}
-				if j < grid.len() - 1 {
-					process(&mut grid, &mut queue, &mut flashes, i - 1, j + 1);
+				if j < WIDTH - 1 {
+					process(i - 1, j + 1);
 				}
 			}
-			if i < grid.len() - 1 {
-				process(&mut grid, &mut queue, &mut flashes, i + 1, j);
+			if i < WIDTH - 1 {
+				process(i + 1, j);
 				if j > 0 {
-					process(&mut grid, &mut queue, &mut flashes, i + 1, j - 1);
+					process(i + 1, j - 1);
 				}
-				if j < grid.len() - 1 {
-					process(&mut grid, &mut queue, &mut flashes, i + 1, j + 1);
+				if j < WIDTH - 1 {
+					process(i + 1, j + 1);
 				}
 			}
 			if j > 0 {
-				process(&mut grid, &mut queue, &mut flashes, i, j - 1);
+				process(i, j - 1);
 			}
-			if j < grid.len() - 1 {
-				process(&mut grid, &mut queue, &mut flashes, i, j + 1);
+			if j < WIDTH - 1 {
+				process(i, j + 1);
 			}
 		}
 		for row in grid.iter_mut() {
 			for octopus in row {
-				if octopus > &mut 9 {
+				if *octopus > 9 {
 					*octopus = 0;
 				}
 			}
@@ -62,46 +67,49 @@ pub fn part2() -> i64 {
 	loop {
 		time += 1;
 		let mut flashes = 0;
-		for i in 0..grid.len() {
-			for j in 0..grid[i].len() {
+		for i in 0..WIDTH {
+			for j in 0..WIDTH {
 				process(&mut grid, &mut queue, &mut flashes, i, j);
 			}
 		}
 		while !queue.is_empty() {
 			let (i, j) = queue.pop().unwrap();
+			let mut process = |i: usize, j: usize| {
+				process(&mut grid, &mut queue, &mut flashes, i, j);
+			};
 			if i > 0 {
-				process(&mut grid, &mut queue, &mut flashes, i - 1, j);
+				process(i - 1, j);
 				if j > 0 {
-					process(&mut grid, &mut queue, &mut flashes, i - 1, j - 1);
+					process(i - 1, j - 1);
 				}
-				if j < grid.len() - 1 {
-					process(&mut grid, &mut queue, &mut flashes, i - 1, j + 1);
+				if j < WIDTH - 1 {
+					process(i - 1, j + 1);
 				}
 			}
-			if i < grid.len() - 1 {
-				process(&mut grid, &mut queue, &mut flashes, i + 1, j);
+			if i < WIDTH - 1 {
+				process(i + 1, j);
 				if j > 0 {
-					process(&mut grid, &mut queue, &mut flashes, i + 1, j - 1);
+					process(i + 1, j - 1);
 				}
-				if j < grid.len() - 1 {
-					process(&mut grid, &mut queue, &mut flashes, i + 1, j + 1);
+				if j < WIDTH - 1 {
+					process(i + 1, j + 1);
 				}
 			}
 			if j > 0 {
-				process(&mut grid, &mut queue, &mut flashes, i, j - 1);
+				process(i, j - 1);
 			}
-			if j < grid.len() - 1 {
-				process(&mut grid, &mut queue, &mut flashes, i, j + 1);
+			if j < WIDTH - 1 {
+				process(i, j + 1);
 			}
 		}
 		for row in grid.iter_mut() {
 			for octopus in row {
-				if octopus > &mut 9 {
+				if *octopus > 9 {
 					*octopus = 0;
 				}
 			}
 		}
-		if flashes == (grid.len() * grid[0].len()) {
+		if flashes == (WIDTH * WIDTH) {
 			return time;
 		}
 	}
