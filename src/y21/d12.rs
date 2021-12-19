3e0eee35e99bba893392ebe2eb0f175baa1257a6
Jonathan Sharman <jonathan.sharman@gmail.com>
2021-12-18 22:14:56 -0600

    Speed up day 12 using vectors instead of hashmaps

--- src/y21/d12.rs
+++ src/y21/d12.rs
@@ -8,55 +8,101 @@ crate::test::test_part!(test1, part1, 4707);
 crate::test::test_part!(test2, part2, 130493);
 
 pub fn part1() -> i64 {
-	count_paths(&read_graph(), &HashMap::new(), true, "start")
+	read_graph().count_paths_from_start(false)
 }
 
 pub fn part2() -> i64 {
-	count_paths(&read_graph(), &HashMap::new(), false, "start")
+	read_graph().count_paths_from_start(true)
 }
 
-type Graph = HashMap<String, HashSet<String>>;
+struct NamedNode {
+	idx: usize,
+	big: bool,
+	neighbors: HashSet<String>,
+}
 
 fn read_graph() -> Graph {
-	let mut graph = HashMap::new();
+	let mut named_nodes = HashMap::new();
+
 	for line in read_lines("input/2021/12.txt") {
-		let split = line.split('-').collect_vec();
-		let (a, b) = (split[0], split[1]);
-		graph
-			.entry(a.to_owned())
-			.or_insert_with(HashSet::new)
-			.insert(b.to_owned());
-		graph
-			.entry(b.to_owned())
-			.or_insert_with(HashSet::new)
-			.insert(a.to_owned());
+		let mut add_edge = |a: &str, b: &str| {
+			let n = named_nodes.len();
+			named_nodes
+				.entry(a.to_owned())
+				.or_insert_with(|| NamedNode {
+					idx: n,
+					big: a.chars().all(char::is_uppercase),
+					neighbors: HashSet::new(),
+				})
+				.neighbors
+				.insert(b.to_owned());
+		};
+
+		let (a, b) = line.split('-').collect_tuple().unwrap();
+		add_edge(a, b);
+		add_edge(b, a);
 	}
-	graph
+
+	let mut nodes = vec![Node::default(); named_nodes.len()];
+	for string_node in named_nodes.values() {
+		nodes[string_node.idx] = Node {
+			big: string_node.big,
+			neighbors: string_node
+				.neighbors
+				.iter()
+				.map(|name| named_nodes[name].idx)
+				.collect(),
+		};
+	}
+
+	Graph {
+		start_idx: named_nodes["start"].idx,
+		end_idx: named_nodes["end"].idx,
+		nodes,
+	}
 }
 
-fn count_paths(
-	graph: &Graph,
-	visit_map: &HashMap<String, u32>,
-	double_used: bool,
-	from: &str,
-) -> i64 {
-	if from == "end" {
-		return 1;
+#[derive(Clone, Default)]
+struct Node {
+	big: bool,
+	neighbors: Vec<usize>,
+}
+
+struct Graph {
+	start_idx: usize,
+	end_idx: usize,
+	nodes: Vec<Node>,
+}
+
+impl Graph {
+	fn count_paths_from_start(&self, allow_double_crossing: bool) -> i64 {
+		self.count_paths(
+			&vec![0; self.nodes.len()],
+			!allow_double_crossing,
+			self.start_idx,
+		)
 	}
-	let mut visit_map = visit_map.clone();
-	let visits = visit_map.entry(from.into()).or_insert(0);
-	*visits += 1;
-	if from.chars().all(char::is_uppercase) || *visits == 1 {
-		graph[from]
-			.iter()
-			.map(|neighbor| count_paths(graph, &visit_map, double_used, neighbor))
-			.sum()
-	} else if *visits == 2 && !double_used && from != "start" {
-		graph[from]
-			.iter()
-			.map(|neighbor| count_paths(graph, &visit_map, true, neighbor))
-			.sum()
-	} else {
-		0
+
+	fn count_paths(&self, visits: &[u32], double_used: bool, from: usize) -> i64 {
+		if from == self.end_idx {
+			return 1;
+		}
+		let mut visits = Vec::from(visits);
+		visits[from] += 1;
+		if self.nodes[from].big || visits[from] == 1 {
+			self.nodes[from]
+				.neighbors
+				.iter()
+				.map(|neighbor| self.count_paths(&visits, double_used, *neighbor))
+				.sum()
+		} else if visits[from] == 2 && !double_used && from != self.start_idx {
+			self.nodes[from]
+				.neighbors
+				.iter()
+				.map(|neighbor| self.count_paths(&visits, true, *neighbor))
+				.sum()
+		} else {
+			0
+		}
 	}
 }
