a8e8c3ffff0acd39ad7653b8431a2260cd3a8101
Jonathan Sharman <jonathan.sharman@gmail.com>
2021-12-20 00:29:49 -0600

    Day 20, part 1

---
+++ src/y21/d20.rs
@@ -0,0 +1,70 @@
+use crate::io::read_lines;
+use itertools::Itertools;
+use std::collections::HashMap;
+
+crate::test::test_part!(test1, part1, ?);
+crate::test::test_part!(test2, part2, ?);
+
+pub fn part1() -> usize {
+	let mut lines = read_lines("input/2021/20.txt");
+	let algo = read_algo(&mut lines);
+	lines.next();
+	let mut image = read_image(&mut lines);
+	for iter in 0..2 {
+		// If the image enhancement algorithm maps 0 to on, then every other
+		// image has an infinite number of light pixels. These must be
+		// represented implicitly, based on the current iteration. If the
+		// algorithm maps 0 to off, then unmapped pixels are simply off.
+		let unmapped_is_on = algo[0] && iter % 2 == 1;
+
+		let mut next_image = HashMap::new();
+		for ((i, j), _) in image.iter() {
+			for i in i - 1..=i + 1 {
+				for j in j - 1..=j + 1 {
+					let mut idx = 0;
+					for k in i - 1..=i + 1 {
+						for l in j - 1..=j + 1 {
+							idx <<= 1;
+							let on = match image.get(&(k, l)) {
+								Some(on) => *on,
+								None => unmapped_is_on,
+							};
+							if on {
+								idx += 1;
+							}
+						}
+					}
+					next_image.insert((i, j), algo[idx]);
+				}
+			}
+		}
+		image = next_image;
+	}
+	image.into_values().filter(|&x| x).count()
+}
+
+pub fn part2() -> usize {
+	let lines = read_lines("input/2021/20.txt");
+	lines.count()
+}
+
+fn read_algo(lines: &mut impl Iterator<Item = String>) -> [bool; 512] {
+	lines
+		.next()
+		.unwrap()
+		.chars()
+		.map(|c| c == '#')
+		.collect_vec()
+		.try_into()
+		.unwrap()
+}
+
+fn read_image(lines: &mut impl Iterator<Item = String>) -> HashMap<(i32, i32), bool> {
+	let mut image = HashMap::new();
+	for (i, line) in lines.enumerate() {
+		for (j, c) in line.chars().enumerate() {
+			image.insert((i as i32, j as i32), c == '#');
+		}
+	}
+	image
+}
