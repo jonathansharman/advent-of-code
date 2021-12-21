83d557bd2541dfd64097b66db6e94047101fe5e8
Jonathan Sharman <jonathan.sharman@gmail.com>
2021-12-21 01:21:14 -0600

    Complete day 21, part 2

--- src/y21/d21.rs
+++ src/y21/d21.rs
@@ -1,43 +1,43 @@
-use itertools::Itertools;
-
 use crate::io::read_lines;
+use itertools::Itertools;
+use std::collections::HashMap;
 
 crate::test::test_part!(test1, part1, 684495);
-crate::test::test_part!(test2, part2, ?);
+crate::test::test_part!(test2, part2, 152587196649184);
 
 pub fn part1() -> u32 {
-	let (mut space1, mut space2) = read_starting_spaces();
-	let (mut score1, mut score2) = (0, 0);
+	let mut spaces = read_starting_spaces();
+	let mut scores = [0, 0];
+	let mut player = 0;
 	let mut die = DeterministicDie { value: 0, rolls: 0 };
 	loop {
-		space1 += die.roll() + die.roll() + die.roll();
-		space1 = (space1 - 1) % 10 + 1;
-		score1 += space1;
-		if score1 >= 1000 {
-			return score2 * die.rolls;
+		spaces[player] += die.roll() + die.roll() + die.roll();
+		spaces[player] = (spaces[player] - 1) % 10 + 1;
+		scores[player] += spaces[player];
+		if scores[player] >= 1000 {
+			return scores[1 - player] * die.rolls;
 		}
-		space2 += die.roll() + die.roll() + die.roll();
-		space2 = (space2 - 1) % 10 + 1;
-		score2 += space2;
-		if score2 >= 1000 {
-			return score1 * die.rolls;
-		}
+		player = 1 - player;
 	}
 }
 
 pub fn part2() -> u64 {
-	let (space1, space2) = read_starting_spaces();
 	let start_state = State {
-		spaces: [space1, space2],
+		spaces: read_starting_spaces(),
 		scores: [0, 0],
 	};
-	*start_state.wins(0).iter().max().unwrap()
+	*start_state
+		.wins(&mut HashMap::new(), 0, 3)
+		.iter()
+		.max()
+		.unwrap()
 }
 
-fn read_starting_spaces() -> (u32, u32) {
+fn read_starting_spaces() -> [u32; 2] {
 	read_lines("input/2021/21.txt")
 		.map(|line| line.chars().last().unwrap().to_digit(10).unwrap())
-		.collect_tuple()
+		.collect_vec()
+		.try_into()
 		.unwrap()
 }
 
@@ -57,7 +57,7 @@ impl DeterministicDie {
 	}
 }
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Eq, Hash)]
 struct State {
 	spaces: [u32; 2],
 	scores: [u32; 2],
@@ -67,36 +67,36 @@ impl State {
 	fn roll(&self, player: usize, roll: u32) -> State {
 		let mut next = self.clone();
 		next.spaces[player] = (next.spaces[player] + roll - 1) % 10 + 1;
-		next.scores[player] += next.spaces[player];
 		next
 	}
 
-	fn wins(&self, player: usize) -> [u64; 2] {
-		let next1 = self.roll(player, 1);
-		let next2 = self.roll(player, 2);
-		let next3 = self.roll(player, 3);
-
+	fn wins(
+		&self,
+		cache: &mut HashMap<(State, usize), [u64; 2]>,
+		player: usize,
+		rolls: u32,
+	) -> [u64; 2] {
 		let mut wins = [0, 0];
-		if next1.scores[player] >= 1000 {
-			wins[player] += 1;
+		if rolls == 0 {
+			if let Some(wins) = cache.get(&(self.clone(), player)) {
+				return *wins;
+			}
+			let mut next = self.clone();
+			next.scores[player] += next.spaces[player];
+			if next.scores[player] >= 21 {
+				wins[player] += 1;
+			} else {
+				let next_wins = next.wins(cache, 1 - player, 3);
+				wins[0] += next_wins[0];
+				wins[1] += next_wins[1];
+			}
+			cache.insert((self.clone(), player), wins);
 		} else {
-			let next_wins = next1.wins(1 - player);
-			wins[0] += next_wins[0];
-			wins[1] += next_wins[1];
-		}
-		if next2.scores[player] >= 1000 {
-			wins[player] += 1;
-		} else {
-			let next_wins = next2.wins(1 - player);
-			wins[0] += next_wins[0];
-			wins[1] += next_wins[1];
-		}
-		if next3.scores[player] >= 1000 {
-			wins[player] += 1;
-		} else {
-			let next_wins = next3.wins(1 - player);
-			wins[0] += next_wins[0];
-			wins[1] += next_wins[1];
+			for roll in 1..=3 {
+				let next_wins = self.roll(player, roll).wins(cache, player, rolls - 1);
+				wins[0] += next_wins[0];
+				wins[1] += next_wins[1];
+			}
 		}
 		wins
 	}
