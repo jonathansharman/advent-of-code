5e9df45340da6ad8be5fb772c1bbe62824a2d1ce
Jonathan Sharman <jonathan.sharman@gmail.com>
2022-01-03 13:02:21 -0600

    Add answer to unit test for 2020-02-2

--- src/y20/d02.rs
+++ src/y20/d02.rs
@@ -2,7 +2,7 @@ use crate::io::read_lines;
 use itertools::Itertools;
 
 crate::test::test_part!(test1, part1, 636);
-crate::test::test_part!(test2, part2, ?);
+crate::test::test_part!(test2, part2, 588);
 
 pub fn part1() -> usize {
 	read_lines("input/2020/02.txt")
