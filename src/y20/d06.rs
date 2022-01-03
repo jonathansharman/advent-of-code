5a8c910537afc047a2efe28a8fa372333bd49462
Jonathan Sharman <jonathan.sharman@gmail.com>
2022-01-03 13:01:38 -0600

    2020-06-2

--- src/y20/d06.rs
+++ src/y20/d06.rs
@@ -1,8 +1,8 @@
 use crate::io::read_lines;
 use std::collections::HashSet;
 
-crate::test::test_part!(test1, part1, ?);
-crate::test::test_part!(test2, part2, ?);
+crate::test::test_part!(test1, part1, 6782);
+crate::test::test_part!(test2, part2, 3596);
 
 pub fn part1() -> usize {
 	let mut sum = 0;
@@ -22,5 +22,24 @@ pub fn part1() -> usize {
 }
 
 pub fn part2() -> usize {
-	read_lines("input/2020/06.txt").count()
+	let mut sum = 0;
+	let mut group = Vec::new();
+	let mut process_group = |group: &mut Vec<String>| {
+		sum += group
+			.iter()
+			.map(|line| HashSet::from_iter(line.chars()))
+			.reduce(|acc: HashSet<_>, chars| acc.intersection(&chars).cloned().collect())
+			.unwrap_or_default()
+			.len();
+		group.clear();
+	};
+	for line in read_lines("input/2020/06.txt") {
+		if line.is_empty() {
+			process_group(&mut group);
+		} else {
+			group.push(line);
+		}
+	}
+	process_group(&mut group);
+	sum
 }
