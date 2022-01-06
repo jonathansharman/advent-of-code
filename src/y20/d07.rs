e3d5c6517c000cc0faa3bcd1676025166a6224b8
Jonathan Sharman <jonathan.sharman@gmail.com>
2022-01-06 01:21:39 -0600

    2020-07-2

--- src/y20/d07.rs
+++ src/y20/d07.rs
@@ -2,31 +2,23 @@ use crate::io::read_lines;
 use std::collections::{HashMap, HashSet};
 
 crate::test::test_part!(test1, part1, 248);
-crate::test::test_part!(test2, part2, ?);
+crate::test::test_part!(test2, part2, 57281);
 
 pub fn part1() -> usize {
-	let contained_bags: HashMap<String, Vec<String>> = read_lines("input/2020/07.txt")
-		.map(|line| {
-			let tokens: Vec<_> = line.split_whitespace().collect();
-			let outer = format!("{} {}", tokens[0], tokens[1]);
-			let mut inner = Vec::new();
-			for i in (4..tokens.len()).step_by(4) {
-				if tokens[i].parse::<u8>().is_ok() {
-					inner.push(format!("{} {}", tokens[i + 1], tokens[i + 2]));
-				}
-			}
-			(outer, inner)
-		})
-		.collect();
+	let contained_bags = get_contained_bags();
 	let mut bags_containing_gold = HashSet::new();
 	for outer in contained_bags.keys() {
-		visit(&contained_bags, &mut bags_containing_gold, outer);
+		contains_gold(&contained_bags, &mut bags_containing_gold, outer);
 	}
 	bags_containing_gold.len()
 }
 
-fn visit(
-	contained_bags: &HashMap<String, Vec<String>>,
+pub fn part2() -> usize {
+	contained_bags_count(&get_contained_bags(), "shiny gold")
+}
+
+fn contains_gold(
+	contained_bags: &HashMap<String, Vec<(usize, String)>>,
 	bags_containing_gold: &mut HashSet<String>,
 	outer: &str,
 ) -> bool {
@@ -34,7 +26,8 @@ fn visit(
 		return true;
 	}
 	for inner in contained_bags.get(outer).unwrap().iter() {
-		if inner == "shiny gold" || visit(contained_bags, bags_containing_gold, inner) {
+		if inner.1 == "shiny gold" || contains_gold(contained_bags, bags_containing_gold, &inner.1)
+		{
 			bags_containing_gold.insert(outer.to_owned());
 			return true;
 		}
@@ -42,6 +35,30 @@ fn visit(
 	false
 }
 
-pub fn part2() -> usize {
-	read_lines("input/2020/07.txt").count()
+fn get_contained_bags() -> HashMap<String, Vec<(usize, String)>> {
+	read_lines("input/2020/07.txt")
+		.map(|line| {
+			let tokens: Vec<_> = line.split_whitespace().collect();
+			let outer = format!("{} {}", tokens[0], tokens[1]);
+			let mut inner = Vec::new();
+			for i in (4..tokens.len()).step_by(4) {
+				if let Ok(count) = tokens[i].parse::<usize>() {
+					inner.push((count, format!("{} {}", tokens[i + 1], tokens[i + 2])));
+				}
+			}
+			(outer, inner)
+		})
+		.collect()
+}
+
+fn contained_bags_count(
+	contained_bags: &HashMap<String, Vec<(usize, String)>>,
+	outer: &str,
+) -> usize {
+	contained_bags
+		.get(outer)
+		.unwrap()
+		.iter()
+		.map(|(count, bag)| count * (1 + contained_bags_count(contained_bags, bag)))
+		.sum()
 }
