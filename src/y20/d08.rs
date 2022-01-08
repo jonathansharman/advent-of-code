762db5788c503b69bcee247d3d303698037a52b4
Jonathan Sharman <jonathan.sharman@gmail.com>
2022-01-07 21:46:08 -0600

    2020-08-2

--- src/y20/d08.rs
+++ src/y20/d08.rs
@@ -1,50 +1,95 @@
 use crate::io::read_lines;
 
 crate::test::test_part!(test1, part1, 1949);
-crate::test::test_part!(test2, part2, ?);
+crate::test::test_part!(test2, part2, 2092);
 
 pub fn part1() -> i64 {
+	exec(&read_program()).err().unwrap()
+}
+
+pub fn part2() -> i64 {
 	let program = read_program();
-	let mut visited = vec![false; program.len()];
-	let mut acc = 0;
-	let mut pc = 0;
-	loop {
-		if visited[pc] {
-			return acc;
-		}
-		visited[pc] = true;
-		match program[pc] {
-			Op::Acc(arg) => {
-				acc += arg;
-				pc += 1;
+	for idx in (0..program.len()).rev() {
+		let op = program[idx];
+		match op.code {
+			Code::Acc => continue,
+			Code::Jmp => {
+				let mut program = program.clone();
+				program[idx] = Op {
+					code: Code::Nop,
+					arg: op.arg,
+				};
+				if let Ok(acc) = exec(&program) {
+					return acc;
+				}
 			}
-			Op::Jmp(arg) => {
-				pc = (pc as i64 + arg) as usize;
-			}
-			Op::Nop => {
-				pc += 1;
+			Code::Nop => {
+				let mut program = program.clone();
+				program[idx] = Op {
+					code: Code::Jmp,
+					arg: op.arg,
+				};
+				if let Ok(acc) = exec(&program) {
+					return acc;
+				}
 			}
 		}
 	}
+	unreachable!()
 }
 
-pub fn part2() -> usize {
-	read_lines("input/2020/08.txt").count()
-}
-
-enum Op {
-	Acc(i64),
-	Jmp(i64),
+#[derive(Clone, Copy)]
+enum Code {
+	Acc,
+	Jmp,
 	Nop,
+}
+
+#[derive(Clone, Copy)]
+struct Op {
+	code: Code,
+	arg: i64,
 }
 
 fn read_program() -> Vec<Op> {
 	read_lines("input/2020/08.txt")
-		.map(|line| match &line[..3] {
-			"acc" => Op::Acc(line[4..].parse().unwrap()),
-			"jmp" => Op::Jmp(line[4..].parse().unwrap()),
-			"nop" => Op::Nop,
-			_ => panic!("bad op"),
+		.map(|line| {
+			let arg = line[4..].parse().unwrap();
+			let code = match &line[..3] {
+				"acc" => Code::Acc,
+				"jmp" => Code::Jmp,
+				"nop" => Code::Nop,
+				_ => panic!("bad op"),
+			};
+			Op { code, arg }
 		})
 		.collect()
+}
+
+fn exec(program: &[Op]) -> Result<i64, i64> {
+	let mut visited = vec![false; program.len()];
+	let mut acc = 0;
+	let mut pc = 0;
+	loop {
+		if pc == program.len() {
+			return Ok(acc);
+		}
+		if visited[pc] {
+			return Err(acc);
+		}
+		visited[pc] = true;
+		let op = program[pc];
+		match op.code {
+			Code::Acc => {
+				acc += op.arg;
+				pc += 1;
+			}
+			Code::Jmp => {
+				pc = (pc as i64 + op.arg) as usize;
+			}
+			Code::Nop => {
+				pc += 1;
+			}
+		}
+	}
 }
