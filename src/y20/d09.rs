6e3697fcb62493faeb79dba2aafdb3454c4bfca5
Jonathan Sharman <jonathan.sharman@gmail.com>
2022-01-08 13:48:32 -0600

    2020-09-2

--- src/y20/d09.rs
+++ src/y20/d09.rs
@@ -1,12 +1,27 @@
+use crate::io::parse_lines;
 use itertools::Itertools;
 
-use crate::io::parse_lines;
-
 crate::test::test_part!(test1, part1, 1309761972);
-crate::test::test_part!(test2, part2, ?);
+crate::test::test_part!(test2, part2, 177989832);
 
-pub fn part1() -> i64 {
-	let numbers: Vec<i64> = parse_lines("input/2020/09.txt").collect();
+pub fn part1() -> u64 {
+	find_invalid(&parse_lines("input/2020/09.txt").collect_vec())
+}
+
+pub fn part2() -> u64 {
+	let numbers: Vec<u64> = parse_lines("input/2020/09.txt").collect_vec();
+	let target = find_invalid(&numbers);
+	for window_size in 2..numbers.len() {
+		for window in numbers.windows(window_size) {
+			if window.iter().sum::<u64>() == target {
+				return window.iter().min().unwrap() + window.iter().max().unwrap();
+			}
+		}
+	}
+	unreachable!()
+}
+
+fn find_invalid(numbers: &[u64]) -> u64 {
 	let window_size = 25;
 	for (k, &n) in numbers.iter().enumerate().skip(window_size) {
 		if numbers[k - window_size..k]
@@ -18,8 +33,4 @@ pub fn part1() -> i64 {
 		}
 	}
 	unreachable!()
-}
-
-pub fn part2() -> usize {
-	0
 }
