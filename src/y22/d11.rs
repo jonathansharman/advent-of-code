b09d6fe331281f173d746cf370faf29397357558
Jonathan Sharman <jonathan.sharman@gmail.com>
2022-12-11 08:48:27 -0500

    2022-11-2 just look away

--- src/y22/d11.rs
+++ src/y22/d11.rs
@@ -1,27 +1,29 @@
 use std::collections::VecDeque;
 
+use itertools::Itertools;
+
 use crate::io::read_lines;
 
 crate::test::test_part!(test1, part1, 62491);
-crate::test::test_part!(test2, part2, ?);
+crate::test::test_part!(test2, part2, 17408399184);
 
 #[derive(Clone, Default, Debug)]
 enum Op {
 	#[default]
 	None,
-	Plus(u32),
-	Times(u32),
+	Plus(u64),
+	Times(u64),
 	Square,
 }
 
 #[derive(Clone, Default, Debug)]
 struct Monkey {
-	items: VecDeque<u32>,
+	items: VecDeque<Vec<u64>>,
 	op: Op,
-	test_divisor: u32,
+	test_divisor: u64,
 	if_true: usize,
 	if_false: usize,
-	inspections: u32,
+	inspections: u64,
 }
 
 fn get_n<T: std::str::FromStr>(line: &str) -> T
@@ -31,7 +33,7 @@ where
 	line.split_whitespace().last().unwrap().parse().unwrap()
 }
 
-pub fn part1() -> u32 {
+fn read_monkeys() -> Vec<Monkey> {
 	let mut monkeys = Vec::new();
 	let mut next_monkey = Monkey::default();
 	for line in read_lines("input/2022/11.txt") {
@@ -42,7 +44,7 @@ pub fn part1() -> u32 {
 				.strip_prefix("  Starting items: ")
 				.unwrap()
 				.split(", ")
-				.map(|x| x.parse().unwrap())
+				.map(|x| vec![x.parse().unwrap()])
 				.collect();
 		} else if line.starts_with("  Operation") {
 			next_monkey.op = if line.contains('*') {
@@ -63,6 +65,11 @@ pub fn part1() -> u32 {
 		}
 	}
 	monkeys.push(next_monkey);
+	monkeys
+}
+
+pub fn part1() -> u64 {
+	let mut monkeys = read_monkeys();
 	for _round in 0..20 {
 		for idx in 0..monkeys.len() {
 			while !monkeys[idx].items.is_empty() {
@@ -70,12 +77,12 @@ pub fn part1() -> u32 {
 				let mut item = monkeys[idx].items.pop_front().unwrap();
 				match monkeys[idx].op {
 					Op::None => (),
-					Op::Plus(n) => item += n,
-					Op::Times(n) => item *= n,
-					Op::Square => item = item.pow(2),
+					Op::Plus(n) => item[0] += n,
+					Op::Times(n) => item[0] *= n,
+					Op::Square => item[0] *= item[0],
 				}
-				item /= 3;
-				let target = if item % monkeys[idx].test_divisor == 0 {
+				item[0] /= 3;
+				let target = if item[0] % monkeys[idx].test_divisor == 0 {
 					monkeys[idx].if_true
 				} else {
 					monkeys[idx].if_false
@@ -88,6 +95,59 @@ pub fn part1() -> u32 {
 	monkeys[0].inspections * monkeys[1].inspections
 }
 
-pub fn part2() -> usize {
-	0
+pub fn part2() -> u64 {
+	let mut monkeys = read_monkeys();
+	let divisors = monkeys
+		.iter()
+		.map(|monkey| monkey.test_divisor)
+		.collect_vec();
+	for monkey in monkeys.iter_mut() {
+		for item in monkey.items.iter_mut() {
+			*item = divisors.iter().map(|d| item[0] % d).collect();
+		}
+	}
+	for _round in 0..10_000 {
+		for idx in 0..monkeys.len() {
+			while !monkeys[idx].items.is_empty() {
+				monkeys[idx].inspections += 1;
+				let mut item = monkeys[idx].items.pop_front().unwrap();
+				match monkeys[idx].op {
+					Op::None => (),
+					Op::Plus(n) => apply_op(&divisors, &mut item, |x| {
+						x.checked_add(n).unwrap()
+					}),
+					Op::Times(n) => apply_op(&divisors, &mut item, |x| {
+						x.checked_mul(n).unwrap()
+					}),
+					Op::Square => apply_op(&divisors, &mut item, |x| {
+						x.checked_mul(x).unwrap()
+					}),
+				}
+				let target = if item[idx] % monkeys[idx].test_divisor == 0 {
+					monkeys[idx].if_true
+				} else {
+					monkeys[idx].if_false
+				};
+				monkeys[target].items.push_back(item);
+			}
+		}
+	}
+	monkeys.sort_by(|m1, m2| m2.inspections.cmp(&m1.inspections));
+	//print_inspections(0, &monkeys);
+	monkeys[0].inspections * monkeys[1].inspections
+}
+
+fn apply_op<F>(divisors: &[u64], item: &mut [u64], op: F)
+where
+	F: Fn(u64) -> u64,
+{
+	for (i, value) in item.iter_mut().enumerate() {
+		*value = op(*value) % divisors[i];
+	}
+}
+
+fn print_inspections(round: usize, monkeys: &[Monkey]) {
+	let is: Vec<_> =
+		monkeys.iter().map(|m| m.inspections).enumerate().collect();
+	println!("{round}: {is:?}");
 }
