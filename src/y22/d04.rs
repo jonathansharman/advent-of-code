2beb8a3f0fae2e0f4c2cdc9c880a936b9b530746
Jonathan Sharman <jonathan.sharman@gmail.com>
2022-12-10 23:48:39 -0500

    2022-04-2

--- src/y22/d04.rs
+++ src/y22/d04.rs
@@ -1,7 +1,7 @@
 use crate::io::read_lines;
 
 crate::test::test_part!(test1, part1, 540);
-crate::test::test_part!(test2, part2, ?);
+crate::test::test_part!(test2, part2, 872);
 
 pub fn part1() -> usize {
 	read_lines("input/2022/04.txt")
@@ -16,5 +16,15 @@ pub fn part1() -> usize {
 }
 
 pub fn part2() -> usize {
-	0
+	read_lines("input/2022/04.txt")
+		.filter(|line| {
+			let bounds: Vec<u32> =
+				line.split(['-', ',']).map(|n| n.parse().unwrap()).collect();
+			let (a, b) = (bounds[0]..=bounds[1], bounds[2]..=bounds[3]);
+			a.contains(b.start())
+				|| a.contains(b.end())
+				|| b.contains(a.start())
+				|| b.contains(a.end())
+		})
+		.count()
 }
