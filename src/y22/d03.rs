a4efe21a6a67137adec44a39a460fd42e9132248
Jonathan Sharman <jonathan.sharman@gmail.com>
2022-12-10 23:26:22 -0500

    2022-03-2

--- src/y22/d03.rs
+++ src/y22/d03.rs
@@ -1,9 +1,19 @@
 use std::collections::HashSet;
 
+use itertools::Itertools;
+
 use crate::io::read_lines;
 
 crate::test::test_part!(test1, part1, 8088);
-crate::test::test_part!(test2, part2, ?);
+crate::test::test_part!(test2, part2, 2522);
+
+fn priority(item: &u8) -> u32 {
+	if (b'A'..=b'Z').contains(item) {
+		(item - b'A' + 27) as u32
+	} else {
+		(item - b'a' + 1) as u32
+	}
+}
 
 pub fn part1() -> u32 {
 	read_lines("input/2022/03.txt")
@@ -12,16 +22,24 @@ pub fn part1() -> u32 {
 			let (first, second) = line.split_at(line.len() / 2);
 			let first = first.iter().collect::<HashSet<&u8>>();
 			let second = second.iter().collect::<HashSet<&u8>>();
-			let dupe = *first.intersection(&second).next().unwrap();
-			if (b'A'..=b'Z').contains(dupe) {
-				(dupe - b'A' + 27) as u32
-			} else {
-				(dupe - b'a' + 1) as u32
-			}
+			priority(first.intersection(&second).next().unwrap())
 		})
 		.sum()
 }
 
-pub fn part2() -> usize {
-	0
+pub fn part2() -> u32 {
+	read_lines("input/2022/03.txt")
+		.chunks(3)
+		.into_iter()
+		.map(|chunk| {
+			let item = chunk
+				.map(|line| line.as_bytes().iter().cloned().collect::<HashSet<u8>>())
+				.reduce(|acc, next| acc.intersection(&next).cloned().collect::<HashSet<u8>>())
+				.unwrap()
+				.into_iter()
+				.next()
+				.unwrap();
+			priority(&item)
+		})
+		.sum()
 }
