c4772fb9f53b1eef702ba5fa04388b32a9e2fbcd
Jonathan Sharman <jonathan.sharman@gmail.com>
2022-12-10 20:54:45 -0500

    2022-12-2

--- src/y22/d01.rs
+++ src/y22/d01.rs
@@ -1,7 +1,9 @@
+use std::collections::BinaryHeap;
+
 use crate::io::read_lines;
 
-crate::test::test_part!(test1, part1, ?);
-crate::test::test_part!(test2, part2, ?);
+crate::test::test_part!(test1, part1, 74711);
+crate::test::test_part!(test2, part2, 209481);
 
 pub fn part1() -> usize {
 	let mut current_total = 0;
@@ -18,5 +20,17 @@ pub fn part1() -> usize {
 }
 
 pub fn part2() -> usize {
-	0
+	let mut current_total = 0;
+	// Suboptimal since pushes are O(lg n) instead of O(k), but good enough.
+	let mut queue = BinaryHeap::new();
+	for line in read_lines("input/2022/01.txt") {
+		if let Ok(calories) = line.parse::<usize>() {
+			current_total += calories;
+		} else {
+			queue.push(current_total);
+			current_total = 0;
+		}
+	}
+	queue.push(current_total);
+	(0..3).map(|_| queue.pop().unwrap()).sum()
 }
