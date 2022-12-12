76c33acbe3d145f41b1fa89ae71366cb00511993
Jonathan Sharman <jonathan.sharman@gmail.com>
2022-12-12 10:46:43 -0500

    Switch to BFS thereby solving the bug somehow
    Must have been a bug in my Dijkstra's implementation

--- src/y22/d12.rs
+++ src/y22/d12.rs
@@ -1,7 +1,4 @@
-use std::{
-	cmp::Reverse,
-	collections::{BinaryHeap, HashMap, HashSet},
-};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 use crate::{io::read_lines, neighbors};
 
@@ -13,6 +10,8 @@ type Coords = (usize, usize);
 struct Map {
 	start: Coords,
 	end: Coords,
+	nrows: usize,
+	ncols: usize,
 	heights: Vec<Vec<u32>>,
 }
 
@@ -39,6 +38,8 @@ fn read_map() -> Map {
 	Map {
 		start,
 		end,
+		nrows: heights.len(),
+		ncols: heights[0].len(),
 		heights,
 	}
 }
@@ -47,10 +48,9 @@ type Graph = HashMap<Coords, HashSet<Coords>>;
 
 fn build_graph(map: &Map) -> Graph {
 	let mut graph = Graph::new();
-	let (nrows, ncols) = (map.heights.len(), map.heights[0].len());
-	for r in 0..nrows {
-		for c in 0..ncols {
-			for (nr, nc) in neighbors::four(nrows, ncols, r, c) {
+	for r in 0..map.nrows {
+		for c in 0..map.ncols {
+			for (nr, nc) in neighbors::four(map.nrows, map.ncols, r, c) {
 				if map.heights[nr][nc] as i32 - map.heights[r][c] as i32 <= 1 {
 					graph
 						.entry((r, c))
@@ -76,33 +76,40 @@ fn flip_graph(graph: Graph) -> Graph {
 	flipped
 }
 
-fn shortest_distances(
+fn bfs(
 	start: Coords,
 	nrows: usize,
 	ncols: usize,
 	graph: &Graph,
-) -> HashMap<Coords, u32> {
-	let mut previous = HashMap::new();
-	let mut distances = HashMap::new();
-	let mut queue = BinaryHeap::new();
+) -> Vec<Vec<u32>> {
+	let mut distances = Vec::new();
+	let mut visited = Vec::new();
 	for r in 0..nrows {
+		distances.push(Vec::new());
+		visited.push(Vec::new());
 		for c in 0..ncols {
-			let distance = if (r, c) == start { 0 } else { u32::MAX };
-			distances.insert((r, c), distance);
-			queue.push((Reverse(distance), (r, c)))
+			if (r, c) == start {
+				distances[r].push(0);
+				visited[r].push(true);
+			} else {
+				distances[r].push(u32::MAX);
+				visited[r].push(false);
+			}
 		}
 	}
-	while let Some((priority, current)) = queue.pop() {
-		if priority.0 != distances[&current] {
-			continue;
-		}
+	let mut queue = VecDeque::new();
+	queue.push_back(start);
+	while let Some(current) = queue.pop_front() {
 		if let Some(neighbors) = graph.get(&current) {
 			for neighbor in neighbors {
-				let candidate = distances[&current] + 1;
-				if candidate < distances[neighbor] {
-					distances.insert(*neighbor, candidate);
-					previous.insert(neighbor, current);
-					queue.push((Reverse(candidate), *neighbor));
+				if visited[neighbor.0][neighbor.1] {
+					continue;
+				}
+				visited[neighbor.0][neighbor.1] = true;
+				let candidate = distances[current.0][current.1] + 1;
+				if candidate < distances[neighbor.0][neighbor.1] {
+					distances[neighbor.0][neighbor.1] = candidate;
+					queue.push_back(*neighbor);
 				}
 			}
 		}
@@ -113,31 +120,20 @@ fn shortest_distances(
 pub fn part1() -> u32 {
 	let map = read_map();
 	let graph = build_graph(&map);
-	shortest_distances(
-		map.start,
-		map.heights.len(),
-		map.heights[0].len(),
-		&graph,
-	)[&map.end]
+	bfs(map.start, map.nrows, map.ncols, &graph)[map.end.0][map.end.1]
 }
 
 pub fn part2() -> u32 {
 	let map = read_map();
 	let graph = flip_graph(build_graph(&map));
-	let distances = shortest_distances(
-		map.end,
-		map.heights.len(),
-		map.heights[0].len(),
-		&graph,
-	);
+	let distances = bfs(map.end, map.nrows, map.ncols, &graph);
 	let mut m = u32::MAX;
-	for r in 0..map.heights.len() {
-		for c in 0..map.heights[r].len() {
-			if map.heights[r][c] == 0 {
-				m = m.min(distances[&(r, c)]);
+	for (r, height_row) in map.heights.iter().enumerate() {
+		for (c, h) in height_row.iter().enumerate() {
+			if *h == 0 {
+				m = m.min(distances[r][c]);
 			}
 		}
 	}
-	println!("{distances:?}");
 	m
 }
