39a0023dcc9e39315768b4aceafed0fc33e8b4c8
Jonathan Sharman <jonathan.sharman@gmail.com>
2022-12-16 22:53:53 -0500

    2022-16-1 - good enough!

--- src/y22/d16.rs
+++ src/y22/d16.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 use itertools::Itertools;
 
@@ -46,43 +46,59 @@ fn get_one_distances(
 }
 
 fn get_all_distances(
-	tunnels: &HashMap<String, Vec<String>>,
+	tunnels: HashMap<String, Vec<String>>,
 ) -> HashMap<String, HashMap<String, i32>> {
 	tunnels
 		.keys()
-		.map(|v| (v.clone(), get_one_distances(tunnels, v)))
+		.map(|v| (v.clone(), get_one_distances(&tunnels, v)))
 		.collect()
 }
 
 const TIME_LIMIT: i32 = 30;
 
-pub fn part1() -> i32 {
-	let (tunnels, rates) = get_tunnels_and_rates();
-	let distances = get_all_distances(&tunnels);
-	rates
-		.keys()
-		.permutations(rates.len())
-		.map(|mut visit_order| {
-			let mut pressure = 0;
-			let mut total_rate = 0;
-			let mut time = 0;
-			let mut location = "AA";
-			loop {
-				let Some(next) = visit_order.pop() else { break };
-				let distance = distances[location][next];
-				let dt = distance + 1;
-				if time + dt > TIME_LIMIT {
-					break;
-				}
-				time += dt;
-				pressure += dt * total_rate;
-				location = next;
-				total_rate += rates[location];
+fn max_pressure(
+	rates: &HashMap<String, i32>,
+	distances: &HashMap<String, HashMap<String, i32>>,
+	time: i32,
+	total_rate: i32,
+	location: &str,
+	remaining_valves: HashSet<String>,
+) -> i32 {
+	remaining_valves
+		.iter()
+		.map(|next| {
+			let dt = distances[location][next] + 1;
+			if time + dt > TIME_LIMIT {
+				return (TIME_LIMIT - time) * total_rate;
 			}
-			pressure + (TIME_LIMIT - time) * total_rate
+			let time = time + dt;
+			let mut child_remaining_values = remaining_valves.clone();
+			child_remaining_values.remove(next);
+			let child_pressure = max_pressure(
+				rates,
+				distances,
+				time,
+				total_rate + rates[next],
+				next,
+				child_remaining_values,
+			);
+			child_pressure + dt * total_rate
 		})
 		.max()
-		.unwrap_or_default()
+		.unwrap_or((TIME_LIMIT - time) * total_rate)
+}
+
+pub fn part1() -> i32 {
+	let (tunnels, rates) = get_tunnels_and_rates();
+	let distances = get_all_distances(tunnels);
+	max_pressure(
+		&rates,
+		&distances,
+		0,
+		0,
+		"AA",
+		rates.keys().cloned().collect(),
+	)
 }
 
 pub fn part2() -> usize {
