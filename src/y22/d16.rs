56bd541a8b85db5a7fa4aae4d01afcafac72b1ef
Jonathan Sharman <jonathan.sharman@gmail.com>
2022-12-17 16:40:20 -0500

    2022-16-2 - very slow but acceptable

--- src/y22/d16.rs
+++ src/y22/d16.rs
@@ -5,7 +5,7 @@ use itertools::Itertools;
 use crate::io::read_lines;
 
 crate::test::test_part!(test1, part1, 1944);
-crate::test::test_part!(test2, part2, ?);
+crate::test::test_part!(test2, part2, 2679);
 
 fn get_tunnels_and_rates(
 ) -> (HashMap<String, Vec<String>>, HashMap<String, i32>) {
@@ -54,9 +54,8 @@ fn get_all_distances(
 		.collect()
 }
 
-const TIME_LIMIT: i32 = 30;
-
 fn max_pressure(
+	time_limit: i32,
 	rates: &HashMap<String, i32>,
 	distances: &HashMap<String, HashMap<String, i32>>,
 	time: i32,
@@ -68,13 +67,14 @@ fn max_pressure(
 		.iter()
 		.map(|next| {
 			let dt = distances[location][next] + 1;
-			if time + dt > TIME_LIMIT {
-				return (TIME_LIMIT - time) * total_rate;
+			if time + dt > time_limit {
+				return (time_limit - time) * total_rate;
 			}
 			let time = time + dt;
 			let mut child_remaining_values = remaining_valves.clone();
 			child_remaining_values.remove(next);
 			let child_pressure = max_pressure(
+				time_limit,
 				rates,
 				distances,
 				time,
@@ -85,13 +85,14 @@ fn max_pressure(
 			child_pressure + dt * total_rate
 		})
 		.max()
-		.unwrap_or((TIME_LIMIT - time) * total_rate)
+		.unwrap_or((time_limit - time) * total_rate)
 }
 
 pub fn part1() -> i32 {
 	let (tunnels, rates) = get_tunnels_and_rates();
 	let distances = get_all_distances(tunnels);
 	max_pressure(
+		30,
 		&rates,
 		&distances,
 		0,
@@ -101,6 +102,25 @@ pub fn part1() -> i32 {
 	)
 }
 
-pub fn part2() -> usize {
-	0
+pub fn part2() -> i32 {
+	let (tunnels, rates) = get_tunnels_and_rates();
+	let distances = get_all_distances(tunnels);
+	let all_valves: HashSet<_> = rates.keys().cloned().collect();
+	all_valves
+		.iter()
+		.powerset()
+		.map(|valves1| {
+			let valves1 = HashSet::from_iter(valves1.into_iter().cloned());
+			let valves2 = all_valves.difference(&valves1).cloned().collect();
+			let time_limit = 26;
+			let p1 = max_pressure(
+				time_limit, &rates, &distances, 0, 0, "AA", valves1,
+			);
+			let p2 = max_pressure(
+				time_limit, &rates, &distances, 0, 0, "AA", valves2,
+			);
+			p1 + p2
+		})
+		.max()
+		.unwrap()
 }
