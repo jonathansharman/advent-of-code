a0e6731930602400b3e8809f29374f6057141260
Jonathan Sharman <jonathan.sharman@gmail.com>
2022-12-21 14:01:12 -0500

    2022-21-2 - not 100% general, a little kludgey

--- src/y22/d21.rs
+++ src/y22/d21.rs
@@ -4,11 +4,11 @@ use itertools::Itertools;
 
 use crate::io::read_lines;
 
-crate::test::test_part!(test1, part1, ?);
-crate::test::test_part!(test2, part2, ?);
+crate::test::test_part!(test1, part1, 93813115694560);
+crate::test::test_part!(test2, part2, 3910938071092);
 
 enum Yell {
-	Number(i64),
+	Number(f64),
 	Sum(String, String),
 	Difference(String, String),
 	Product(String, String),
@@ -20,7 +20,7 @@ fn get_yells() -> HashMap<String, Yell> {
 		.map(|line| {
 			let (monkey, yell) = line.split_once(": ").unwrap();
 			let monkey = monkey.to_owned();
-			if let Ok(n) = yell.parse::<i64>() {
+			if let Ok(n) = yell.parse::<f64>() {
 				(monkey, Yell::Number(n))
 			} else {
 				let (l, op, r) = yell.split(' ').collect_tuple().unwrap();
@@ -38,10 +38,10 @@ fn get_yells() -> HashMap<String, Yell> {
 }
 
 fn evaluate(
-	cache: &mut HashMap<String, i64>,
+	cache: &mut HashMap<String, f64>,
 	yells: &HashMap<String, Yell>,
 	monkey: String,
-) -> i64 {
+) -> f64 {
 	if let Some(n) = cache.get(&monkey) {
 		return *n;
 	}
@@ -71,9 +71,39 @@ fn evaluate(
 pub fn part1() -> i64 {
 	let yells = get_yells();
 	let mut cache = HashMap::new();
-	evaluate(&mut cache, &yells, "root".to_string())
+	evaluate(&mut cache, &yells, "root".to_string()) as i64
 }
 
-pub fn part2() -> usize {
-	0
+pub fn part2() -> i64 {
+	let mut yells = get_yells();
+	let root_name = "root".to_string();
+	let current_root_yell = yells.remove(&root_name).unwrap();
+	yells.insert(
+		root_name,
+		match current_root_yell {
+			Yell::Number(_) => panic!("Expected binary expression"),
+			Yell::Sum(l, r) => Yell::Difference(l, r),
+			difference @ Yell::Difference(_, _) => difference,
+			Yell::Product(l, r) => Yell::Difference(l, r),
+			Yell::Quotient(l, r) => Yell::Difference(l, r),
+		},
+	);
+	// These bounds as well as the slope of the search space are hard-coded for
+	// my input (which has a negative slope). A general solution would require
+	// first inferring the upper and lower bounds and the slope.
+	let mut min: i64 = 0;
+	let mut max: i64 = 2 << 42;
+	while min <= max {
+		let humn = ((min + max) / 2) as f64;
+		yells.insert("humn".to_string(), Yell::Number(humn));
+		let mut cache = HashMap::new();
+		let root = evaluate(&mut cache, &yells, "root".to_string());
+		println!("{min}-{max}: {root}");
+		match root.partial_cmp(&0.0).unwrap() {
+			std::cmp::Ordering::Less => max = ((min + max) / 2) - 1,
+			std::cmp::Ordering::Equal => return humn as i64,
+			std::cmp::Ordering::Greater => min = ((min + max) / 2) + 1,
+		}
+	}
+	panic!("Answer not found");
 }
