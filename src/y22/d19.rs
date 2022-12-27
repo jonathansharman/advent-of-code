1b94a4d6ba8fc0a3eb2897afd166b7385c47305d
Jonathan Sharman <jonathan.sharman@gmail.com>
2022-12-27 11:16:59 -0500

    Vastly speed up part 1 - probably still not good enough for part 2

--- src/y22/d19.rs
+++ src/y22/d19.rs
@@ -17,36 +17,48 @@ struct Factory<'b> {
 }
 
 impl<'b> Factory<'b> {
-	fn max_geodes(mut self, build: Option<usize>) -> usize {
+	fn max_geodes(mut self, build: usize) -> usize {
+		// Determine the amount of time required to acquire resources to build
+		// the target bot.
+		let required_resources = self.blueprint[build];
+		let mut wait = 0;
+		for (i, req) in required_resources.into_iter().enumerate() {
+			if req > self.inv[i] {
+				if self.bots[i] == 0 {
+					// No bots available to build a required resource.
+					// Impossible to build this bot next.
+					return self.inv[3];
+				}
+				let deficit = req - self.inv[i];
+				// Divide rounding up:
+				wait = wait.max((deficit + self.bots[i] - 1) / self.bots[i]);
+			}
+		}
+		if self.time_left < wait {
+			return self.inv[3];
+		}
+		// Produce enough resources to build the bot.
+		self.time_left -= wait;
+		for (i, resource) in self.inv.iter_mut().enumerate() {
+			*resource += self.bots[i] * wait;
+		}
+		// Build the bot on the next turn, producing resources first.
 		if self.time_left == 0 {
 			return self.inv[3];
 		}
 		self.time_left -= 1;
-		// Produce.
 		for (i, resource) in self.inv.iter_mut().enumerate() {
 			*resource += self.bots[i];
 		}
-		// Build.
-		if let Some(build) = build {
-			for (i, c) in self.inv.iter_mut().zip(self.blueprint[build].iter())
-			{
-				*i -= c;
-			}
-			self.bots[build] += 1;
+		for (i, c) in self.inv.iter_mut().zip(self.blueprint[build].iter()) {
+			*i -= c;
 		}
-		// Execute all possible moves (including doing nothing).
-		let mut best = 0;
-		for build in 0..4 {
-			if self
-				.inv
-				.iter()
-				.zip(self.blueprint[build].iter())
-				.all(|(i, c)| i >= c)
-			{
-				best = best.max(self.clone().max_geodes(Some(build)))
-			}
-		}
-		best.max(self.max_geodes(None))
+		self.bots[build] += 1;
+		// Try building all possible bots next and choose the best outcome.
+		(0..4)
+			.map(|build| self.clone().max_geodes(build))
+			.max()
+			.unwrap()
 	}
 }
 
@@ -60,14 +72,18 @@ fn max_geodes(time_limit: usize, blueprint: &Blueprint) -> usize {
 		inv: [0; 4],
 		bots: [1, 0, 0, 0],
 	};
-	factory.max_geodes(None)
+	(0..4)
+		.map(|build| factory.clone().max_geodes(build))
+		.max()
+		.unwrap()
 }
 
 pub fn part1() -> usize {
-	let start = std::time::Instant::now();
+	let overall_start = std::time::Instant::now();
 	read_lines("input/2022/19.txt")
 		.enumerate()
 		.map(|(i, line)| {
+			let start = std::time::Instant::now();
 			let numbers: Vec<usize> = line
 				.split(|c: char| !c.is_ascii_digit())
 				.filter_map(|s| {
@@ -85,18 +101,21 @@ pub fn part1() -> usize {
 				[numbers[5], 0, numbers[6], 0],
 			];
 			let quality = (i + 1) * max_geodes(TIME_LIMIT_1, &blueprint);
-			let elapsed = (std::time::Instant::now() - start).as_secs_f32();
-			println!("Quality {i}: {quality} ({elapsed}s)");
+			let now = std::time::Instant::now();
+			let elapsed = (now - start).as_secs_f32();
+			let total_elapsed = (now - overall_start).as_secs_f32();
+			println!("Quality {i}: {quality} ({elapsed}s / {total_elapsed}s)");
 			quality
 		})
 		.sum()
 }
 
 pub fn part2() -> usize {
-	let start = std::time::Instant::now();
+	let overall_start = std::time::Instant::now();
 	read_lines("input/2022/19.txt")
 		.take(3)
 		.map(|line| {
+			let start = std::time::Instant::now();
 			let numbers: Vec<usize> = line
 				.split(|c: char| !c.is_ascii_digit())
 				.filter_map(|s| {
@@ -114,8 +133,10 @@ pub fn part2() -> usize {
 				[numbers[5], 0, numbers[6], 0],
 			];
 			let m = max_geodes(TIME_LIMIT_2, &blueprint);
-			let elapsed = (std::time::Instant::now() - start).as_secs_f32();
-			println!("Max geodes: {m} ({elapsed}s)");
+			let now = std::time::Instant::now();
+			let elapsed = (now - start).as_secs_f32();
+			let total_elapsed = (now - overall_start).as_secs_f32();
+			println!("Max geodes: {m} ({elapsed}s / {total_elapsed}s)");
 			m
 		})
 		.product()
