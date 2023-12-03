6a0119d3f358d08124fd5e9fb01f0950831779ad
Jonathan Sharman <jonathan.sharman@gmail.com>
2023-12-03 18:39:14 -0500

    2022-24-1

--- src/y22/d24.rs
+++ src/y22/d24.rs
@@ -1,151 +1,131 @@
-use std::collections::HashMap;
+use std::collections::{HashSet, VecDeque};
+
+use num::integer::lcm;
 
 use crate::io::read_lines;
 
-crate::test::test_part!(test1, part1, ?);
+crate::test::test_part!(test1, part1, 334);
 crate::test::test_part!(test2, part2, ?);
 
-struct Blizzards {
-	blizzards: Vec<[bool; 4]>,
-	width: i32,
-	height: i32,
+enum Blizzard {
+	None,
+	Up,
+	Down,
+	Left,
+	Right,
 }
+
+struct Blizzards(Vec<Vec<Blizzard>>);
 
 impl Blizzards {
 	fn load() -> Blizzards {
 		let lines = read_lines("input/2022/24.txt").collect::<Vec<_>>();
-		let blizzards_vecs = lines[1..lines.len() - 1]
-			.iter()
-			.map(|line| {
-				line.chars()
-					.filter_map(|c| match c {
-						'^' => Some([true, false, false, false]),
-						'v' => Some([false, true, false, false]),
-						'<' => Some([false, false, true, false]),
-						'>' => Some([false, false, false, true]),
-						'#' => None,
-						_ => Some([false; 4]),
-					})
-					.collect::<Vec<_>>()
-			})
-			.collect::<Vec<_>>();
-		let width = blizzards_vecs[0].len() as i32;
-		let height = blizzards_vecs.len() as i32;
-		let blizzards = Vec::from_iter(
-			blizzards_vecs.into_iter().flat_map(|v| v.into_iter()),
-		);
-		Blizzards {
-			blizzards,
-			width,
-			height,
-		}
+		Blizzards(
+			lines[1..lines.len() - 1]
+				.iter()
+				.map(|line| {
+					line.chars()
+						.filter_map(|c| match c {
+							'^' => Some(Blizzard::Up),
+							'v' => Some(Blizzard::Down),
+							'<' => Some(Blizzard::Left),
+							'>' => Some(Blizzard::Right),
+							'#' => None,
+							_ => Some(Blizzard::None),
+						})
+						.collect::<Vec<_>>()
+				})
+				.collect::<Vec<_>>(),
+		)
 	}
 
-	fn open(&self, coords: [i32; 2], time: i32) -> bool {
-		if coords == [-1, 0] || coords == [self.height, self.width - 1] {
-			return true;
-		}
-		if coords[0] < 0
-			|| coords[0] >= self.height
-			|| coords[1] < 0
-			|| coords[1] >= self.width
-		{
-			return false;
-		}
-		let idx = ((coords[0] + time) % self.height) * self.width + coords[1];
-		let southerly = self.blizzards[idx as usize][0];
-		let idx = ((((coords[0] - time) % self.height) + self.height)
-			% self.height)
-			* self.width + coords[1];
-		let northerly = self.blizzards[idx as usize][1];
-		let idx = coords[0] * self.width + (coords[1] + time) % self.width;
-		let easterly = self.blizzards[idx as usize][2];
-		let idx = coords[0] * self.width
-			+ ((coords[1] - time) % self.width + self.width) % self.width;
-		let westerly = self.blizzards[idx as usize][3];
-		!southerly && !northerly && !easterly && !westerly
+	fn height(&self) -> usize {
+		self.0.len()
 	}
 
-	fn time_to_exit(
-		&self,
-		cache: &mut HashMap<([i32; 2], i32), Option<usize>>,
-		coords: [i32; 2],
-		time: i32,
-		time_limit: i32,
-	) -> Option<usize> {
-		if coords == [self.height, self.width - 1] {
-			let result = Some(0);
-			cache.insert((coords, time), result);
-			result
-		} else if !self.open(coords, time) || time == time_limit {
-			cache.insert((coords, time), None);
-			None
-		} else {
-			let wait = self
-				.time_to_exit(cache, coords, time + 1, time_limit)
-				.map(|v| v + 1);
-			let north = self
-				.time_to_exit(
-					cache,
-					[coords[0] - 1, coords[1]],
-					time + 1,
-					time_limit,
-				)
-				.map(|v| v + 1);
-			let south = self
-				.time_to_exit(
-					cache,
-					[coords[0] + 1, coords[1]],
-					time + 1,
-					time_limit,
-				)
-				.map(|v| v + 1);
-			let west = self
-				.time_to_exit(
-					cache,
-					[coords[0], coords[1] - 1],
-					time + 1,
-					time_limit,
-				)
-				.map(|v| v + 1);
-			let east = self
-				.time_to_exit(
-					cache,
-					[coords[0], coords[1] + 1],
-					time + 1,
-					time_limit,
-				)
-				.map(|v| v + 1);
+	fn width(&self) -> usize {
+		self.0[0].len()
+	}
 
-			let result = wait
-				.unwrap_or(usize::MAX)
-				.min(north.unwrap_or(usize::MAX))
-				.min(south.unwrap_or(usize::MAX))
-				.min(west.unwrap_or(usize::MAX))
-				.min(east.unwrap_or(usize::MAX));
-			let result = if result == usize::MAX {
-				None
-			} else {
-				Some(result)
-			};
-			cache.insert((coords, time), result);
-			result
+	fn period(&self) -> usize {
+		lcm(self.width(), self.height())
+	}
+
+	// Minute -> row -> column -> "is this space open at that time"
+	fn get_open(&self) -> Vec<Vec<Vec<bool>>> {
+		let mut result = Vec::new();
+		for time in 0..self.period() {
+			let mut t = vec![vec![true; self.width()]; self.height()];
+			for (i, row) in self.0.iter().enumerate() {
+				for (j, bliz) in row.iter().enumerate() {
+					match bliz {
+						Blizzard::None => {}
+						Blizzard::Up => {
+							t[(i + self.height() - (time % self.height()))
+								% self.height()][j] = false
+						}
+						Blizzard::Down => {
+							t[(i + time) % self.height()][j] = false
+						}
+						Blizzard::Left => {
+							t[i][(j + self.width() - (time % self.width()))
+								% self.width()] = false
+						}
+						Blizzard::Right => {
+							t[i][(j + time) % self.width()] = false
+						}
+					}
+				}
+			}
+			result.push(t);
 		}
+		result
 	}
 }
 
 pub fn part1() -> usize {
 	let blizzards = Blizzards::load();
-	let mut cache = HashMap::new();
-	for time_limit in blizzards.width + blizzards.height.. {
-		println!("Time limit: {time_limit}");
-		if let Some(time) =
-			blizzards.time_to_exit(&mut cache, [-1, 0], 0, time_limit)
-		{
-			return time;
+	let open = blizzards.get_open();
+	let mut visited = HashSet::new();
+
+	let mut queue = VecDeque::new();
+	// TODO: This assumes the solution requires moving into the blizzards at
+	// minute 1, but it could potentially require moving later.
+	queue.push_back((1, 0, 0));
+	while let Some((t, i, j)) = queue.pop_front() {
+		if visited.contains(&(t, i, j)) {
+			continue;
+		}
+		// Check if we're one away from the exit.
+		if i == blizzards.height() - 1 && j == blizzards.width() - 1 {
+			return t + 1;
+		}
+		// Mark these spatio-temporal coordinates as visited.
+		visited.insert((t, i, j));
+		// Get the open spaces at the next minute.
+		let next_open = &open[(t + 1) % blizzards.period()];
+		// Wait.
+		if next_open[i][j] {
+			queue.push_back((t + 1, i, j));
+		}
+		// Go up.
+		if i > 0 && next_open[i - 1][j] {
+			queue.push_back((t + 1, i - 1, j));
+		}
+		// Go down.
+		if i < blizzards.height() - 1 && next_open[i + 1][j] {
+			queue.push_back((t + 1, i + 1, j));
+		}
+		// Go left.
+		if j > 0 && next_open[i][j - 1] {
+			queue.push_back((t + 1, i, j - 1));
+		}
+		// Go right.
+		if j < blizzards.width() - 1 && next_open[i][j + 1] {
+			queue.push_back((t + 1, i, j + 1));
 		}
 	}
-	panic!("No answer found");
+	panic!("end unreachable");
 }
 
 pub fn part2() -> usize {
