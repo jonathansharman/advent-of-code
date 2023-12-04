d34b8bfc92cc7233d3d511296403524425f59698
Jonathan Sharman <jsharman@niche.com>
2023-12-04 08:40:07 -0500

    2023-04-1

---
+++ src/y23/d04.rs
@@ -0,0 +1,31 @@
+use std::collections::HashSet;
+
+use crate::io::read_lines;
+
+crate::test::test_part!(test1, part1, 21959);
+crate::test::test_part!(test2, part2, ?);
+
+pub fn part1() -> usize {
+	read_lines("input/2023/04.txt")
+		.map(|line| {
+			let line = &line[line.find(':').unwrap() + 1..];
+			let (winners, have) = line.split_once('|').unwrap();
+			let have: HashSet<usize> = HashSet::from_iter(
+				have.split_whitespace().map(|s| s.parse::<usize>().unwrap()),
+			);
+			let n_wins = winners
+				.split_whitespace()
+				.filter(|s| have.contains(&s.parse().unwrap()))
+				.count();
+			if n_wins == 0 {
+				0
+			} else {
+				2u32.pow((n_wins - 1) as u32) as usize
+			}
+		})
+		.sum()
+}
+
+pub fn part2() -> usize {
+	0
+}
