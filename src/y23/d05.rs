263e8672893f690d97c29c51688fc949c594f5e3
Jonathan Sharman <jsharman@niche.com>
2023-12-05 00:38:33 -0500

    2023-05-1

---
+++ src/y23/d05.rs
@@ -0,0 +1,71 @@
+use crate::io::read_lines;
+
+crate::test::test_part!(test1, part1, 484023871);
+crate::test::test_part!(test2, part2, ?);
+
+struct RangeMap {
+	dst: usize,
+	src: usize,
+	len: usize,
+}
+
+impl RangeMap {
+	fn map(&self, n: usize) -> Option<usize> {
+		if (self.src..self.src + self.len).contains(&n) {
+			Some(self.dst + n - self.src)
+		} else {
+			None
+		}
+	}
+}
+
+pub fn part1() -> usize {
+	let mut lines = read_lines("input/2023/05.txt");
+	let seeds = lines
+		.next()
+		.unwrap()
+		.split_whitespace()
+		.skip(1)
+		.map(|s| s.parse().unwrap())
+		.collect::<Vec<usize>>();
+	lines.next();
+	// source → (destination, length)
+	let mut maps = Vec::new();
+	while lines.next().is_some() {
+		let mut map = Vec::new();
+		for line in lines.by_ref() {
+			if line.is_empty() {
+				break;
+			}
+			let numbers = line
+				.split_whitespace()
+				.map(|s| s.parse().unwrap())
+				.collect::<Vec<_>>();
+			map.push(RangeMap {
+				dst: numbers[0],
+				src: numbers[1],
+				len: numbers[2],
+			});
+		}
+		maps.push(map);
+	}
+	seeds
+		.into_iter()
+		.map(|mut src| {
+			for map in maps.iter() {
+				for range_map in map.iter() {
+					if let Some(dst) = range_map.map(src) {
+						src = dst;
+						break;
+					}
+				}
+			}
+			src
+		})
+		.min()
+		.unwrap()
+}
+
+pub fn part2() -> usize {
+	0
+}
