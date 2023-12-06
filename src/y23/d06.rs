b3cf04d425dddf5116d5352fdc06d34b71e4f1de
Jonathan Sharman <jsharman@niche.com>
2023-12-06 00:25:41 -0500

    2023-06: add answers, simplify part 1 a little

--- src/y23/d06.rs
+++ src/y23/d06.rs
@@ -1,27 +1,22 @@
 use crate::io::read_lines;
 
-crate::test::test_part!(test1, part1, ?);
-crate::test::test_part!(test2, part2, ?);
+crate::test::test_part!(test1, part1, 449550);
+crate::test::test_part!(test2, part2, 28360140);
 
 pub fn part1() -> usize {
-	let mut lines = read_lines("input/2023/06.txt");
-	let ts = lines
-		.next()
-		.unwrap()
-		.split_whitespace()
-		.skip(1)
-		.map(|n| n.parse().unwrap())
-		.collect::<Vec<usize>>();
-	let ds = lines
-		.next()
-		.unwrap()
-		.split_whitespace()
-		.skip(1)
-		.map(|n| n.parse().unwrap())
-		.collect::<Vec<usize>>();
-	ts.into_iter()
-		.zip(ds.into_iter())
-		.map(|(t, d)| (0..=t).filter(|hold| hold * (t - hold) > d).count())
+	let lines = read_lines("input/2023/06.txt")
+		.map(|line| {
+			line.split_whitespace()
+				.skip(1)
+				.map(|n| n.parse().unwrap())
+				.collect::<Vec<usize>>()
+		})
+		.collect::<Vec<_>>();
+	let ts = &lines[0];
+	let ds = &lines[1];
+	ts.iter()
+		.zip(ds.iter())
+		.map(|(t, d)| (0..=*t).filter(|hold| hold * (t - hold) > *d).count())
 		.product()
 }
 
