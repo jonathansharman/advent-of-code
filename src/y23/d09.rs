86be7c342b3ffc31b645bca2fe6fd1f6a8aa45e7
Jonathan Sharman <jsharman@niche.com>
2023-12-09 00:27:27 -0500

    2023-09-2

--- src/y23/d09.rs
+++ src/y23/d09.rs
@@ -1,7 +1,7 @@
 use crate::io::read_lines;
 
 crate::test::test_part!(test1, part1, 1921197370);
-crate::test::test_part!(test2, part2, ?);
+crate::test::test_part!(test2, part2, 1124);
 
 fn prediction(history: Vec<i64>) -> i64 {
 	let mut rows = vec![history];
@@ -38,6 +38,37 @@ pub fn part1() -> i64 {
 		.sum()
 }
 
-pub fn part2() -> usize {
-	0
+fn prediction2(history: Vec<i64>) -> i64 {
+	let mut rows = vec![history];
+	loop {
+		let last = rows.last_mut().unwrap();
+		if last.iter().all(|n| *n == 0) {
+			last.insert(0, 0);
+			break;
+		}
+		let mut next = Vec::new();
+		for i in 0..last.len() - 1 {
+			next.push(last[i + 1] - last[i]);
+		}
+		rows.push(next);
+	}
+	let n = rows.len();
+	for i in (0..n - 1).rev() {
+		let i_first = *rows[i].first().unwrap();
+		let i_plus_1_first = *rows[i + 1].first().unwrap();
+		rows[i].insert(0, i_first - i_plus_1_first);
+	}
+	*rows[0].first().unwrap()
+}
+
+pub fn part2() -> i64 {
+	read_lines("input/2023/09.txt")
+		.map(|line| {
+			let history = line
+				.split_whitespace()
+				.map(|n| n.parse().unwrap())
+				.collect::<Vec<_>>();
+			prediction2(history)
+		})
+		.sum()
 }
