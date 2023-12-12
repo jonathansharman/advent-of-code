872f22a4dca60171d8f3df6c75ea5a43a738c475
Jonathan Sharman <jsharman@niche.com>
2023-12-12 15:31:21 -0500

    2023-12 cleanup

--- src/y23/d12.rs
+++ src/y23/d12.rs
@@ -22,7 +22,7 @@ impl Spring {
 	}
 }
 
-pub fn part1() -> usize {
+fn parse_input() -> Vec<(Vec<Spring>, Vec<usize>)> {
 	read_lines("input/2023/12.txt")
 		.map(|line| {
 			let (springs, groups) = line.split_once(' ').unwrap();
@@ -33,41 +33,68 @@ pub fn part1() -> usize {
 					'#' => Spring::Damaged,
 					_ => Spring::Unknown,
 				})
-				.collect::<Vec<_>>();
+				.collect();
 			let groups = groups
 				.split(',')
 				.map(|n| n.parse::<usize>().unwrap())
-				.collect::<Vec<_>>();
-			arrangements(&mut HashMap::new(), 0, 0, &springs, &groups)
+				.collect();
+			(springs, groups)
+		})
+		.collect()
+}
+
+pub fn part1() -> usize {
+	parse_input()
+		.into_iter()
+		.map(|(springs, groups)| {
+			arrangements(&mut HashMap::new(), &springs, &groups)
+		})
+		.sum()
+}
+
+pub fn part2() -> usize {
+	parse_input()
+		.into_iter()
+		.map(|(springs, groups)| {
+			let mut unfolded_springs = springs.clone();
+			for _ in 0..4 {
+				unfolded_springs.push(Spring::Unknown);
+				unfolded_springs.extend(springs.iter());
+			}
+			let mut unfolded_groups = groups.clone();
+			for _ in 0..4 {
+				unfolded_groups.extend(groups.iter());
+			}
+			arrangements(
+				&mut HashMap::new(),
+				&unfolded_springs,
+				&unfolded_groups,
+			)
 		})
 		.sum()
 }
 
 fn arrangements(
 	cache: &mut HashMap<(usize, usize), usize>,
-	i: usize,
-	j: usize,
 	springs: &[Spring],
 	groups: &[usize],
 ) -> usize {
-	let s = &springs[i..];
-	let g = &groups[j..];
-	let key = (springs.len() - i, groups.len() - j);
+	let key = (springs.len(), groups.len());
 	if let Some(result) = cache.get(&key) {
 		return *result;
 	}
-	if g.is_empty() {
+	if groups.is_empty() {
 		// All of the remaining springs must be operational.
-		let result = s.iter().all(Spring::operational) as usize;
+		let result = springs.iter().all(Spring::operational) as usize;
 		cache.insert(key, result);
 		return result;
 	}
-	if s.len() < g.iter().sum::<usize>() + g.len() - 1 {
+	if springs.len() < groups.iter().sum::<usize>() + groups.len() - 1 {
 		// The groups definitely don't fit.
 		cache.insert(key, 0);
 		return 0;
 	}
-	match s.len().cmp(&g[0]) {
+	match springs.len().cmp(&groups[0]) {
 		// Can't fit the next group into the remaining springs.
 		Ordering::Less => {
 			cache.insert(key, 0);
@@ -75,66 +102,33 @@ fn arrangements(
 		}
 		// All springs must be damaged, and there must be exactly one group.
 		Ordering::Equal => {
-			let result =
-				(s.iter().all(Spring::damaged) && g.len() == 1) as usize;
+			let result = (springs.iter().all(Spring::damaged)
+				&& groups.len() == 1) as usize;
 			cache.insert(key, result);
 			result
 		}
 		Ordering::Greater => {
 			let mut result = 0;
 			// Add arrangements with first group at the start.
-			let group_fits = s[..g[0]].iter().all(Spring::damaged);
-			let gap_after_group = s[g[0]].operational();
+			let group_fits = springs[..groups[0]].iter().all(Spring::damaged);
+			let gap_after_group = springs[groups[0]].operational();
 			if group_fits && gap_after_group {
 				// Add the arrangements of the remaining springs and groups.
-				let rest =
-					arrangements(cache, i + g[0] + 1, j + 1, springs, groups);
+				let rest = arrangements(
+					cache,
+					&springs[groups[0] + 1..],
+					&groups[1..],
+				);
 				if rest > 0 {
 					result += rest;
 				}
 			}
 			// Add arrangements with first group after the start.
-			if s[0].operational() {
-				result += arrangements(cache, i + 1, j, springs, groups);
+			if springs[0].operational() {
+				result += arrangements(cache, &springs[1..], groups);
 			}
 			cache.insert(key, result);
 			result
 		}
 	}
-}
-
-pub fn part2() -> usize {
-	read_lines("input/2023/12.txt")
-		.map(|line| {
-			let (springs, groups) = line.split_once(' ').unwrap();
-			let springs = springs
-				.chars()
-				.map(|c| match c {
-					'.' => Spring::Operational,
-					'#' => Spring::Damaged,
-					_ => Spring::Unknown,
-				})
-				.collect::<Vec<_>>();
-			let mut unfolded_springs = springs.clone();
-			for _ in 0..4 {
-				unfolded_springs.push(Spring::Unknown);
-				unfolded_springs.extend(springs.iter());
-			}
-			let groups = groups
-				.split(',')
-				.map(|n| n.parse::<usize>().unwrap())
-				.collect::<Vec<_>>();
-			let mut unfolded_groups = groups.clone();
-			for _ in 0..4 {
-				unfolded_groups.extend(groups.iter());
-			}
-			arrangements(
-				&mut HashMap::new(),
-				0,
-				0,
-				&unfolded_springs,
-				&unfolded_groups,
-			)
-		})
-		.sum()
 }
