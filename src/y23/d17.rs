92cccd6d4082c307efaf8e5fba52dab8959cf4b6
Jonathan Sharman <jsharman@niche.com>
2023-12-18 13:44:20 -0500

    2023-17: DRY up part 1 and 2

--- src/y23/d17.rs
+++ src/y23/d17.rs
@@ -5,12 +5,31 @@ use crate::io::read_lines;
 crate::test::test_part!(test1, part1, 970);
 crate::test::test_part!(test2, part2, 1149);
 
+pub fn part1() -> usize {
+	let costs = read_costs();
+	let adj = get_adjacencies(&costs, 1, 3);
+	shortest_path(&costs, &adj, 0).min(shortest_path(&costs, &adj, 1))
+}
+
+pub fn part2() -> usize {
+	let costs = read_costs();
+	let adj = get_adjacencies(&costs, 4, 10);
+	shortest_path(&costs, &adj, 0).min(shortest_path(&costs, &adj, 1))
+}
+
+fn read_costs() -> Vec<Vec<usize>> {
+	read_lines("input/2023/17.txt")
+		.map(|line| line.bytes().map(|b| (b - b'0') as usize).collect())
+		.collect()
+}
+
 type Coords = (usize, usize, usize);
 
-pub fn part1() -> usize {
-	let costs: Vec<Vec<usize>> = read_lines("input/2023/17.txt")
-		.map(|line| line.bytes().map(|b| (b - b'0') as usize).collect())
-		.collect();
+fn get_adjacencies(
+	costs: &[Vec<usize>],
+	min: usize,
+	max: usize,
+) -> HashMap<Coords, Vec<(Coords, usize)>> {
 	let (n, m) = (costs.len(), costs[0].len());
 	// Model the graph as two planes. Each move must move 1-3 spaces vertically
 	// or horizontally (depending on the current plane) and move to the other
@@ -18,29 +37,39 @@ pub fn part1() -> usize {
 	let mut adj: HashMap<Coords, Vec<(Coords, usize)>> = HashMap::new();
 	for i in 0..n {
 		for j in 0..m {
+			let adj0 = adj.entry((i, j, 0)).or_default();
 			let mut cost = 0;
-			for ii in (i.saturating_sub(3)..i).rev() {
+			for ii in (i.saturating_sub(max)..i).rev() {
 				cost += costs[ii][j];
-				adj.entry((i, j, 0)).or_default().push(((ii, j, 1), cost));
+				if i.abs_diff(ii) >= min {
+					adj0.push(((ii, j, 1), cost));
+				}
 			}
 			let mut cost = 0;
-			for ii in i + 1..=(i + 3).min(n - 1) {
+			for ii in i + 1..=(i + max).min(n - 1) {
 				cost += costs[ii][j];
-				adj.entry((i, j, 0)).or_default().push(((ii, j, 1), cost));
+				if i.abs_diff(ii) >= min {
+					adj0.push(((ii, j, 1), cost));
+				}
+			}
+			let adj1 = adj.entry((i, j, 1)).or_default();
+			let mut cost = 0;
+			for jj in (j.saturating_sub(max)..j).rev() {
+				cost += costs[i][jj];
+				if j.abs_diff(jj) >= min {
+					adj1.push(((i, jj, 0), cost));
+				}
 			}
 			let mut cost = 0;
-			for jj in (j.saturating_sub(3)..j).rev() {
+			for jj in j + 1..=(j + max).min(m - 1) {
 				cost += costs[i][jj];
-				adj.entry((i, j, 1)).or_default().push(((i, jj, 0), cost));
-			}
-			let mut cost = 0;
-			for jj in j + 1..=(j + 3).min(m - 1) {
-				cost += costs[i][jj];
-				adj.entry((i, j, 1)).or_default().push(((i, jj, 0), cost));
+				if j.abs_diff(jj) >= min {
+					adj1.push(((i, jj, 0), cost));
+				}
 			}
 		}
 	}
-	shortest_path(&costs, &adj, 0).min(shortest_path(&costs, &adj, 1))
+	adj
 }
 
 #[derive(PartialEq, Eq)]
@@ -100,50 +129,4 @@ fn shortest_path(
 		}
 	}
 	unreachable!();
-}
-
-pub fn part2() -> usize {
-	let costs: Vec<Vec<usize>> = read_lines("input/2023/17.txt")
-		.map(|line| line.bytes().map(|b| (b - b'0') as usize).collect())
-		.collect();
-	let (n, m) = (costs.len(), costs[0].len());
-	// Model the graph as two planes. Each move must move 1-3 spaces vertically
-	// or horizontally (depending on the current plane) and move to the other
-	// plane, to force alternating horizontal and vertical move sequences.
-	let mut adj: HashMap<Coords, Vec<(Coords, usize)>> = HashMap::new();
-	for i in 0..n {
-		for j in 0..m {
-			let adj0 = adj.entry((i, j, 0)).or_default();
-			let mut cost = 0;
-			for ii in (i.saturating_sub(10)..i).rev() {
-				cost += costs[ii][j];
-				if i.abs_diff(ii) >= 4 {
-					adj0.push(((ii, j, 1), cost));
-				}
-			}
-			let mut cost = 0;
-			for ii in i + 1..=(i + 10).min(n - 1) {
-				cost += costs[ii][j];
-				if i.abs_diff(ii) >= 4 {
-					adj0.push(((ii, j, 1), cost));
-				}
-			}
-			let adj1 = adj.entry((i, j, 1)).or_default();
-			let mut cost = 0;
-			for jj in (j.saturating_sub(10)..j).rev() {
-				cost += costs[i][jj];
-				if j.abs_diff(jj) >= 4 {
-					adj1.push(((i, jj, 0), cost));
-				}
-			}
-			let mut cost = 0;
-			for jj in j + 1..=(j + 10).min(m - 1) {
-				cost += costs[i][jj];
-				if j.abs_diff(jj) >= 4 {
-					adj1.push(((i, jj, 0), cost));
-				}
-			}
-		}
-	}
-	shortest_path(&costs, &adj, 0).min(shortest_path(&costs, &adj, 1))
 }
