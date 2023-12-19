fde347cec12de1c7ed96b25e3be7ada2ca4afef5
Jonathan Sharman <jsharman@niche.com>
2023-12-19 00:00:12 -0500

    2023-18: use Pick's theorem

--- src/y23/d18.rs
+++ src/y23/d18.rs
@@ -5,125 +5,77 @@ use crate::io::read_lines;
 crate::test::test_part!(test1, part1, 36679);
 crate::test::test_part!(test2, part2, 88007104020978);
 
-#[derive(Clone, Copy)]
-struct Instruction {
-	dir: (i64, i64),
-	len: i64,
-}
-
-impl Instruction {
-	fn parse1(s: String) -> Instruction {
-		let parts = s.split_whitespace().collect::<Vec<_>>();
-		let dir = match parts[0] {
-			"U" => (0, 1),
-			"D" => (0, -1),
-			"R" => (1, 0),
-			"L" => (-1, 0),
-			_ => panic!("invalid direction"),
-		};
-		let len = parts[1].parse().unwrap();
-		Instruction { dir, len }
-	}
-
-	fn parse2(s: String) -> Instruction {
-		let start = s.find('#').unwrap() + 1;
-		let len = i64::from_str_radix(
-			&s.chars().skip(start).take(5).collect::<String>(),
-			16,
-		)
-		.unwrap();
-		let dir = match s.chars().nth(start + 5).unwrap() {
-			'0' => (1, 0),
-			'1' => (0, -1),
-			'2' => (-1, 0),
-			'3' => (0, 1),
-			_ => panic!("invalid direction"),
-		};
-		Instruction { dir, len }
-	}
-}
-
 pub fn part1() -> i64 {
 	let instructions = read_lines("input/2023/18.txt")
-		.map(Instruction::parse1)
+		.map(parse1)
 		.collect::<Vec<_>>();
 	area(&instructions)
 }
 
 pub fn part2() -> i64 {
-	let instructions = read_lines("input/2023/18.txt")
-		.map(Instruction::parse2)
+	let vectors = read_lines("input/2023/18.txt")
+		.map(parse2)
 		.collect::<Vec<_>>();
-	area(&instructions)
+	area(&vectors)
 }
 
-fn area(instructions: &[Instruction]) -> i64 {
-	// Use the winding number to tell whether the loop is counter-clockwise.
-	let winding_ccw = instructions
-		.iter()
-		.tuple_windows()
-		.map(|(a, b)| a.dir.0 * b.dir.1 - a.dir.1 * b.dir.0)
-		.sum::<i64>()
-		> 0;
-	let mut coords = (0, 0);
-	let mut vertices = Vec::new();
-	// Find the vertices of the exterior.
-	for (i, a) in instructions.iter().copied().enumerate() {
-		let b = instructions[(i + 1) % instructions.len()];
-		let turn_ccw = (a.dir.0 * b.dir.1 - a.dir.1 * b.dir.0) > 0;
-		coords.0 += a.len * a.dir.0;
-		coords.1 += a.len * a.dir.1;
-		let (x, y) = coords;
-		// TODO: Find an expression to unify all these cases.
-		let v = match (a.dir, winding_ccw, turn_ccw) {
-			// Right, CW loop, right turn
-			((1, 0), false, false) => (x + 1, y + 1),
-			// Right, CW loop, left turn
-			((1, 0), false, true) => (x, y + 1),
-			// Right, CCW loop, right turn
-			((1, 0), true, false) => (x, y),
-			// Right, CCW loop, left turn
-			((1, 0), true, true) => (x + 1, y),
-
-			// Left, CW loop, right turn
-			((-1, 0), false, false) => (x, y),
-			// Left, CW loop, left turn
-			((-1, 0), false, true) => (x + 1, y),
-			// Left, CCW loop, right turn
-			((-1, 0), true, false) => (x + 1, y + 1),
-			// Left, CCW loop, left turn
-			((-1, 0), true, true) => (x, y + 1),
-
-			// Up, CW loop, right turn
-			((0, 1), false, false) => (x, y + 1),
-			// Up, CW loop, left turn
-			((0, 1), false, true) => (x, y),
-			// Up, CCW loop, right turn
-			((0, 1), true, false) => (x + 1, y),
-			// Up, CCW loop, left turn
-			((0, 1), true, true) => (x + 1, y + 1),
-
-			// Down, CW loop, right turn
-			((0, -1), false, false) => (x + 1, y),
-			// Down, CW loop, left turn
-			((0, -1), false, true) => (x + 1, y + 1),
-			// Down, CCW loop, right turn
-			((0, -1), true, false) => (x, y + 1),
-			// Down, CCW loop, left turn
-			((0, -1), true, true) => (x, y),
-
-			_ => unreachable!(),
-		};
-		vertices.push(v);
+fn parse1(s: String) -> (i64, i64) {
+	let parts = s.split_whitespace().collect::<Vec<_>>();
+	let len = parts[1].parse().unwrap();
+	match parts[0] {
+		"U" => (0, len),
+		"D" => (0, -len),
+		"R" => (len, 0),
+		"L" => (-len, 0),
+		_ => panic!("invalid direction"),
 	}
-	// Add the first vertex to the end so we can use tuple windows to iterate
-	// over all edges.
-	vertices.push(vertices[0]);
-	// Use the shoelace formula to find the area.
-	vertices
+}
+
+fn parse2(s: String) -> (i64, i64) {
+	let start = s.find('#').unwrap() + 1;
+	let len = i64::from_str_radix(
+		&s.chars().skip(start).take(5).collect::<String>(),
+		16,
+	)
+	.unwrap();
+	match s.chars().nth(start + 5).unwrap() {
+		'0' => (len, 0),
+		'1' => (0, -len),
+		'2' => (-len, 0),
+		'3' => (0, len),
+		_ => panic!("invalid direction"),
+	}
+}
+
+fn area(vectors: &[(i64, i64)]) -> i64 {
+	// Use the shoelace formula to find the area of the polygon with vertices
+	// centered on the holes.
+	let a = vectors
+		.iter()
+		.copied()
+		.fold(vec![(0, 0)], |mut acc, (vx, vy)| {
+			let (x, y) = acc.last().unwrap();
+			acc.push((x + vx, y + vy));
+			acc
+		})
 		.into_iter()
 		.tuple_windows()
 		.map(|((x1, y1), (x2, y2))| (y1 + y2) * (x1 - x2))
 		.sum::<i64>()
-		.abs() / 2
+		.abs() / 2;
+
+	// Pick's theorem states that for a polygon whose vertices all have integer
+	// coordinates, A = i + b/2 - 1, where:
+	//   1. A is the area of the polygon.
+	//   2. i is the number of interior points with integer coordinates.
+	//   3. b is the number of boundary points with integer coordinates.
+	//
+	// The area of the polygon enclosing the holes (as opposed to just passing
+	// through their centers) is equal to the sum of the interior and boundary
+	// points with integer coordinates of the smaller polygon. Rearranging
+	// Pick's theorem, we have i + b = A + b/2 + 1. We have computed A and can
+	// compute b as the perimeter of the smaller polygon. (There's no need to
+	// compute i explicitly.)
+	let b: i64 = vectors.iter().map(|v| v.0.abs() + v.1.abs()).sum();
+	a + b / 2 + 1
 }
