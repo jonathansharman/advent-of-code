d30210e4c64027a63a92390ff08984b492141694
Jonathan Sharman <jsharman@niche.com>
2023-12-23 00:01:09 -0500

    2023-21-2 WIP

--- src/y23/d21.rs
+++ src/y23/d21.rs
@@ -1,14 +1,13 @@
-use std::{
-	cmp::Ordering,
-	collections::{hash_map::Entry, HashMap, HashSet, VecDeque},
-};
+use std::collections::{hash_map::Entry, HashMap, HashSet, VecDeque};
 
 use itertools::Itertools;
 
 use crate::{io::read_lines, neighbors};
 
 crate::test::test_part!(test1, part1, 3733);
-crate::test::test_part!(test2, part2, ?); // 617687757554990 is too low
+// 617689228309913 is too low
+// 702322399865956 is an upper bound (no walls)
+crate::test::test_part!(test2, part2, ?);
 
 fn read_grid() -> Vec<Vec<bool>> {
 	read_lines("input/2023/21.txt")
@@ -17,6 +16,7 @@ fn read_grid() -> Vec<Vec<bool>> {
 }
 
 const TOTAL_STEPS_1: usize = 64;
+const PARITY_1: usize = TOTAL_STEPS_1 % 2;
 
 pub fn part1() -> usize {
 	let grid = read_grid();
@@ -30,7 +30,7 @@ pub fn part1() -> usize {
 		if !visited.insert((i, j)) {
 			continue;
 		}
-		if d % 2 == 0 {
+		if d % 2 == PARITY_1 {
 			count += 1;
 		}
 		for n in neighbors::four(grid.len(), grid[0].len(), i, j) {
@@ -43,85 +43,95 @@ pub fn part1() -> usize {
 }
 
 const TOTAL_STEPS_2: usize = 26_501_365;
+const PARITY_2: usize = TOTAL_STEPS_2 % 2;
 
 pub fn part2() -> usize {
 	let grid = read_grid();
 	let diameter = grid.len();
 	let radius = diameter / 2;
-	let reachable_counts: HashMap<(usize, usize), HashMap<usize, usize>> =
-		[0, radius, 2 * radius]
-			.into_iter()
-			.cartesian_product([0, radius, 2 * radius])
-			.map(|start| {
-				let distance_counts: HashMap<usize, usize> = get_distances(
-					&grid, start,
-				)
-				.into_values()
-				.fold(HashMap::new(), |mut acc, d| {
-					*acc.entry(d).or_default() += 1;
-					acc
-				});
-				(start, distance_counts)
-			})
-			.collect();
-	// TODO: It should be possible to just iterate over the most-distant
-	// reachable tiles and compute the contributions of the interior tiles
-	// analytically. (There would be two cases for interior tiles, depending on
-	// tile coordinate parity.)
-	let max_tile_idx = TOTAL_STEPS_2 as i64 / diameter as i64;
-	let mut count = 0;
-	let reachable_even = reachable_counts[&(0, 0)]
-		.iter()
-		.filter_map(|(&d, count)| if d % 2 == 1 { Some(count) } else { None })
-		.sum::<usize>();
-	let reachable_odd = reachable_counts[&(0, 0)]
-		.iter()
-		.filter_map(|(&d, count)| if d % 2 == 0 { Some(count) } else { None })
-		.sum::<usize>();
-	for i in -max_tile_idx..=max_tile_idx {
-		println!("{i}");
-		for j in -max_tile_idx..=max_tile_idx {
-			let tile_distance = i.abs() + j.abs();
-			match tile_distance.cmp(&max_tile_idx) {
-				Ordering::Less => {
-					count += if tile_distance % 2 == 0 {
-						reachable_even
-					} else {
-						reachable_odd
-					};
-					continue;
-				}
-				Ordering::Greater => continue,
-				Ordering::Equal => {}
+	let critical_points = (0..=2)
+		.cartesian_product(0..=2)
+		.map(|(i, j)| {
+			let coords = (i * radius, j * radius);
+			let degrees_of_freedom = i.abs_diff(1) + j.abs_diff(1);
+			let zone_distance = degrees_of_freedom * (radius + 1);
+			let distances = get_distances(&grid, coords);
+			CriticalPoint {
+				zone_distance,
+				degrees_of_freedom,
+				distances,
 			}
-			let i_distance_to_start = if i == 0 {
-				0
-			} else {
-				diameter * (i.abs() - 1) as usize + radius
-			};
-			let j_distance_to_start = if j == 0 {
-				0
-			} else {
-				diameter * (j.abs() - 1) as usize + radius
-			};
-			let distance_to_start = i_distance_to_start + j_distance_to_start;
-			let remaining_distance = TOTAL_STEPS_2 - distance_to_start;
-			let parity = remaining_distance % 2;
-			let i_start = (i.signum() + 1) as usize * radius;
-			let j_start = (j.signum() + 1) as usize * radius;
-			count += reachable_counts[&(i_start, j_start)]
-				.iter()
-				.filter_map(|(&d, count)| {
-					if d <= remaining_distance && d % 2 == parity {
-						Some(count)
-					} else {
-						None
+		})
+		.collect::<Vec<_>>();
+	let mut count = 0;
+	for p in critical_points {
+		for d in p.distances.into_values() {
+			let remaining = TOTAL_STEPS_2
+				.saturating_sub(d)
+				.saturating_sub(p.zone_distance);
+			// n = tile distance to farthest reachable tile.
+			let n = remaining / diameter;
+			let d_parity = d % 2;
+			match p.degrees_of_freedom {
+				0 => {
+					if d_parity == PARITY_2 {
+						count += 1;
 					}
-				})
-				.sum::<usize>();
+				}
+				1 => {
+					if diameter % 2 == 0 {
+						// When the diameter is even, the parity of every tile
+						// is the same as the primary tile.
+						if d_parity == PARITY_2 {
+							count += n;
+						}
+					} else {
+						// When the diameter is odd, parity alternates, and
+						// approximately half the reachable tiles will have the
+						// correct parity. If d has the incorrect parity in the
+						// primary tile, then it will start on the correct
+						// parity when extending out, in which case we need to
+						// round up instead of down if the number of reachable
+						// tiles is odd.
+						let round_up = (d_parity != PARITY_2) as usize;
+						count += (n + round_up) / 2;
+					}
+				}
+				2 => {
+					// The relevant tiles are those between 2 and n tiles away.
+					// (The remaining tiles are covered by other zones.)
+					if diameter % 2 == 0 {
+						// Parity is the same across all tiles.
+						if d_parity == PARITY_2 {
+							// Triangular number of tiles.
+							count += (n - 1) * n / 2;
+						}
+					} else {
+						// TODO: Calculable analytically, without a loop.
+
+						// Parity alternates per diagonal row of tiles, starting
+						// with the same parity as the primary tile.
+						for tile_distance in (2 + d_parity..=n).step_by(2) {
+							count += tile_distance - 1;
+						}
+					}
+				}
+				_ => unreachable!(),
+			}
 		}
 	}
 	count
+}
+
+/// Info about one of the nine "important" points on the grid: the center and
+/// the center +/- the radius on each axis.
+struct CriticalPoint {
+	/// Number of directions in this point's zone.
+	degrees_of_freedom: usize,
+	/// Distance from the start to this point's zone.
+	zone_distance: usize,
+	/// Distances from the point to every other point within the primary tile.
+	distances: HashMap<(usize, usize), usize>,
 }
 
 fn get_distances(
