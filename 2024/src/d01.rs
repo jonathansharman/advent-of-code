a639b218bdadb84064a3798de1f98bd5f19ced85
Jonathan Sharman <jonathan.sharman@gmail.com>
2024-12-01 00:43:51 -0500

    2024-01 cleanup

--- 2024/src/d01.rs
+++ 2024/src/d01.rs
@@ -1,55 +1,38 @@
 use std::collections::HashMap;
 
 use aoc::io::read_lines;
+use itertools::Itertools;
 
 aoc::test::test_part!(test1, part1, 2066446);
 aoc::test::test_part!(test2, part2, 24931009);
 
-pub fn part1() -> u32 {
-	let pairs = read_lines("input/01.txt")
-		.map(|line| {
-			let parts = line.split_whitespace().collect::<Vec<_>>();
-			(
-				parts[0].parse::<i32>().unwrap(),
-				parts[1].parse::<i32>().unwrap(),
-			)
-		})
-		.collect::<Vec<_>>();
-	let mut first = pairs.iter().map(|(a, _)| a).copied().collect::<Vec<_>>();
-	first.sort();
-	let mut second = pairs.iter().map(|(_, b)| b).copied().collect::<Vec<_>>();
-	second.sort();
-	first
-		.into_iter()
-		.zip(second)
-		.map(|(a, b)| a.abs_diff(b))
-		.sum()
+fn parse_line(line: String) -> (u32, u32) {
+	line.split_whitespace()
+		.map(|n| n.parse().unwrap())
+		.next_tuple()
+		.unwrap()
 }
 
-pub fn part2() -> i32 {
-	let pairs = read_lines("input/01.txt")
-		.map(|line| {
-			let parts = line.split_whitespace().collect::<Vec<_>>();
-			(
-				parts[0].parse::<i32>().unwrap(),
-				parts[1].parse::<i32>().unwrap(),
-			)
-		})
-		.collect::<Vec<_>>();
-	let count = pairs.iter().map(|(_, b)| b).fold(
-		HashMap::new(),
-		|mut acc: HashMap<i32, i32>, v: &i32| {
-			*acc.entry(*v).or_default() += 1;
-			acc
-		},
-	);
-	pairs
-		.iter()
-		.map(|(a, _)| a)
-		.map(|a| {
-			let c = *count.get(a).unwrap_or(&0);
-			println!("{a}: {c}");
-			a * c
-		})
+pub fn part1() -> u32 {
+	let (mut a, mut b): (Vec<u32>, Vec<u32>) =
+		read_lines("input/01.txt").map(parse_line).unzip();
+	a.sort();
+	b.sort();
+	a.into_iter().zip(b).map(|(a, b)| a.abs_diff(b)).sum()
+}
+
+pub fn part2() -> u32 {
+	let (values, counts): (Vec<u32>, HashMap<u32, u32>) =
+		read_lines("input/01.txt").map(parse_line).fold(
+			(Vec::new(), HashMap::new()),
+			|(mut values, mut counts), (a, b)| {
+				values.push(a);
+				*counts.entry(b).or_default() += 1;
+				(values, counts)
+			},
+		);
+	values
+		.into_iter()
+		.map(|v| v * *counts.get(&v).unwrap_or(&0))
 		.sum()
 }
