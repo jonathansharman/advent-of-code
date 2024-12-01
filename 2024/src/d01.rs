211fd4d66daa2445a53f5a02d0f6ed159400c528
Jonathan Sharman <jonathan.sharman@gmail.com>
2024-12-01 00:22:22 -0500

    2024-01-2

--- 2024/src/d01.rs
+++ 2024/src/d01.rs
@@ -1,7 +1,9 @@
+use std::collections::HashMap;
+
 use aoc::io::read_lines;
 
-aoc::test::test_part!(test1, part1, ?);
-aoc::test::test_part!(test2, part2, ?);
+aoc::test::test_part!(test1, part1, 2066446);
+aoc::test::test_part!(test2, part2, 24931009);
 
 pub fn part1() -> u32 {
 	let pairs = read_lines("input/01.txt")
@@ -24,6 +26,30 @@ pub fn part1() -> u32 {
 		.sum()
 }
 
-pub fn part2() -> usize {
-	read_lines("input/01.txt").count()
+pub fn part2() -> i32 {
+	let pairs = read_lines("input/01.txt")
+		.map(|line| {
+			let parts = line.split_whitespace().collect::<Vec<_>>();
+			(
+				parts[0].parse::<i32>().unwrap(),
+				parts[1].parse::<i32>().unwrap(),
+			)
+		})
+		.collect::<Vec<_>>();
+	let count = pairs.iter().map(|(_, b)| b).fold(
+		HashMap::new(),
+		|mut acc: HashMap<i32, i32>, v: &i32| {
+			*acc.entry(*v).or_default() += 1;
+			acc
+		},
+	);
+	pairs
+		.iter()
+		.map(|(a, _)| a)
+		.map(|a| {
+			let c = *count.get(a).unwrap_or(&0);
+			println!("{a}: {c}");
+			a * c
+		})
+		.sum()
 }
