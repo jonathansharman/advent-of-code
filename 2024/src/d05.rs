ddc6063aae2df347843f266cfa0af60390b22562
Jonathan Sharman <jonathan.sharman@niche.com>
2024-12-05 01:01:18 -0500

    A little 2024-05 cleanup

--- 2024/src/d05.rs
+++ 2024/src/d05.rs
@@ -5,59 +5,56 @@ use aoc::io::read_lines;
 aoc::test::test_part!(test1, part1, 5747);
 aoc::test::test_part!(test2, part2, 5502);
 
-fn get_less(
-	rules: &mut impl Iterator<Item = String>,
-) -> HashSet<(String, String)> {
-	let mut less: HashSet<(String, String)> = HashSet::new();
-	for rule in rules {
-		if rule.is_empty() {
-			break;
-		}
-		let (a, b) = rule.split_once('|').unwrap();
-		less.insert((a.to_owned(), b.to_owned()));
-	}
-	less
+fn get_less(rules: &mut impl Iterator<Item = String>) -> HashSet<(u32, u32)> {
+	rules
+		.map_while(|rule| {
+			if rule.is_empty() {
+				None
+			} else {
+				let (a, b) = rule.split_once('|').unwrap();
+				Some((a.parse().unwrap(), b.parse().unwrap()))
+			}
+		})
+		.collect()
 }
 
-pub fn part1() -> usize {
+pub fn part1() -> u32 {
 	let mut lines = read_lines("input/05.txt");
 	let less = get_less(lines.by_ref());
 	lines
 		.map(|update| {
-			let pages: Vec<String> =
-				update.split(',').map(|s| s.to_owned()).collect();
+			let pages: Vec<u32> =
+				update.split(',').map(|s| s.parse().unwrap()).collect();
 			for (i, a) in pages.iter().enumerate() {
 				for b in pages.iter().skip(i) {
-					if less.contains(&(b.clone(), a.clone())) {
+					if less.contains(&(*b, *a)) {
 						return 0;
 					}
 				}
 			}
-			pages[pages.len() / 2].parse().unwrap()
+			pages[pages.len() / 2]
 		})
 		.sum()
 }
 
-pub fn part2() -> usize {
+pub fn part2() -> u32 {
 	let mut lines = read_lines("input/05.txt");
 	let less = get_less(lines.by_ref());
 	lines
 		.map(|update| {
-			let mut pages: Vec<String> =
-				update.split(',').map(|s| s.to_owned()).collect();
-			for i in 0..pages.len() - 1 {
-				let a = &pages[i];
-				for j in i + 1..pages.len() {
-					let b = pages[j].clone();
-					if less.contains(&(b, a.clone())) {
+			let mut pages: Vec<u32> =
+				update.split(',').map(|s| s.parse().unwrap()).collect();
+			for (i, a) in pages.iter().copied().enumerate() {
+				for b in pages.iter().skip(i).copied() {
+					if less.contains(&(b, a)) {
 						pages.sort_by(|a, b| {
-							if less.contains(&(a.clone(), b.clone())) {
+							if less.contains(&(*a, *b)) {
 								Ordering::Less
 							} else {
 								Ordering::Greater
 							}
 						});
-						return pages[pages.len() / 2].parse().unwrap();
+						return pages[pages.len() / 2];
 					}
 				}
 			}
