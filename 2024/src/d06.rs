acba925ca284fa8f4d929d1f5f73b3a2af129b03
Jonathan Sharman <jonathan.sharman@niche.com>
2024-12-06 01:46:00 -0500

    2024-06-2 (slow)

--- 2024/src/d06.rs
+++ 2024/src/d06.rs
@@ -1,48 +1,94 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
 use aoc::io::read_lines;
 
 aoc::test::test_part!(test1, part1, 5067);
-aoc::test::test_part!(test2, part2, ?);
+aoc::test::test_part!(test2, part2, 1793);
 
-pub fn part1() -> usize {
-	let mut guard: (i32, i32) = (0, 0);
-	let mut dir: (i32, i32) = (-1, 0);
-	let tiles: HashMap<(i32, i32), char> = read_lines("input/06.txt")
+type Point = (i32, i32);
+type Tiles = Vec<Vec<char>>;
+
+fn read() -> (Tiles, Point) {
+	let mut guard: Point = (0, 0);
+	let tiles: Vec<Vec<char>> = read_lines("input/06.txt")
 		.enumerate()
-		.flat_map(|(i, line)| {
+		.map(|(i, line)| {
 			line.chars()
 				.enumerate()
 				.map(|(j, c)| {
-					let coords = (i as i32, j as i32);
-					let c = if c == '^' {
-						guard = coords;
+					if c == '^' {
+						guard = (i as i32, j as i32);
 						'.'
 					} else {
 						c
-					};
-					(coords, c)
+					}
 				})
-				.collect::<Vec<_>>()
+				.collect()
 		})
 		.collect();
+	(tiles, guard)
+}
+
+fn escape_time(
+	tiles: &Tiles,
+	mut guard: Point,
+	obstruction: Option<Point>,
+) -> Option<usize> {
+	let mut dir: Point = (-1, 0);
 	let mut visited = HashSet::new();
-	while let Some(tile) = tiles.get(&guard) {
-		if *tile == '#' {
+	while let Some(tile) = tiles
+		.get(guard.0 as usize)
+		.and_then(|row| row.get(guard.1 as usize))
+	{
+		if visited.contains(&(guard, dir)) {
+			// Cycle.
+			return None;
+		}
+		if *tile == '#' || obstruction == Some(guard) {
 			// Turn.
 			guard.0 -= dir.0;
 			guard.1 -= dir.1;
 			dir = (dir.1, -dir.0)
 		} else {
-			visited.insert(guard);
+			visited.insert((guard, dir));
 		}
 		// Walk.
 		guard.0 += dir.0;
 		guard.1 += dir.1;
 	}
-	visited.len()
+	Some(
+		visited
+			.into_iter()
+			.map(|(guard, _)| guard)
+			.collect::<HashSet<_>>()
+			.len(),
+	)
+}
+
+pub fn part1() -> usize {
+	let (tiles, guard) = read();
+	escape_time(&tiles, guard, None).unwrap()
 }
 
 pub fn part2() -> usize {
-	0
+	let (tiles, guard) = read();
+	tiles
+		.iter()
+		.enumerate()
+		.map(|(i, row)| {
+			row.iter()
+				.enumerate()
+				.filter(|&(j, tile)| {
+					let obstacle = (i as i32, j as i32);
+					obstacle != guard
+						&& *tile != '#' && escape_time(
+						&tiles,
+						guard,
+						Some(obstacle),
+					)
+					.is_none()
+				})
+				.count()
+		})
+		.sum()
 }
