dda3ac14abcb06f96fe7de085e1298618ab31bf1
Jonathan Sharman <jonathan.sharman@niche.com>
2024-12-07 00:38:21 -0500

    2024-07-2

--- 2024/src/d07.rs
+++ 2024/src/d07.rs
@@ -1,7 +1,7 @@
 use aoc::io::read_lines;
 
 aoc::test::test_part!(test1, part1, 882304362421);
-aoc::test::test_part!(test2, part2, ?);
+aoc::test::test_part!(test2, part2, 145149066755184);
 
 struct Equation {
 	lhs: i64,
@@ -37,6 +37,30 @@ pub fn part1() -> i64 {
 		.sum()
 }
 
-pub fn part2() -> usize {
-	0
+fn has_solution_2(lhs: i64, rhs: &[i64]) -> bool {
+	let (&last, rest) = rhs.split_last().unwrap();
+	if rest.is_empty() {
+		lhs == last
+	} else {
+		if has_solution_2(lhs - last, rest)
+			|| (lhs % last == 0 && has_solution_2(lhs / last, rest))
+		{
+			return true;
+		}
+		lhs.to_string()
+			.strip_suffix(&last.to_string())
+			.and_then(|stripped| {
+				stripped.parse().ok().map(|lhs| has_solution_2(lhs, rest))
+			})
+			.unwrap_or_default()
+	}
+}
+
+pub fn part2() -> i64 {
+	read_lines("input/07.txt")
+		.filter_map(|line| {
+			let equation: Equation = line.as_str().into();
+			has_solution_2(equation.lhs, &equation.rhs).then_some(equation.lhs)
+		})
+		.sum()
 }
