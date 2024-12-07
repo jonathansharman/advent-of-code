29957dc7ffee516d68f38f4f25e19ac2e52b8b29
Jonathan Sharman <jonathan.sharman@niche.com>
2024-12-07 13:37:12 -0500

    2020-18-2

--- 2020/src/d18.rs
+++ 2020/src/d18.rs
@@ -1,7 +1,7 @@
 use aoc::io::read_lines;
 
 aoc::test::test_part!(test1, part1, 45283905029161);
-aoc::test::test_part!(test2, part2, ?);
+aoc::test::test_part!(test2, part2, 216975281211165);
 
 enum Expr {
 	Number(u64),
@@ -62,6 +62,57 @@ pub fn part1() -> u64 {
 		.sum()
 }
 
-pub fn part2() -> usize {
-	0
+fn parse_2(line: &str) -> Expr {
+	parse_expr_2(&line.bytes().filter(|b| *b != b' ').collect::<Vec<_>>())
+		.unwrap()
+		.0
+}
+
+fn parse_expr_2(tokens: &[u8]) -> Option<(Expr, &[u8])> {
+	let (mut expr, mut rest) = parse_factor(tokens)?;
+	while let Some((op, after_op)) = rest.split_first() {
+		match op {
+			b'*' => {
+				let (term, after_term) = parse_factor(after_op)?;
+				expr = Expr::Product(Box::new((expr, term)));
+				rest = after_term;
+			}
+			_ => break,
+		}
+	}
+	Some((expr, rest))
+}
+
+fn parse_factor(tokens: &[u8]) -> Option<(Expr, &[u8])> {
+	let (mut expr, mut rest) = parse_term_2(tokens)?;
+	while let Some((op, after_op)) = rest.split_first() {
+		match op {
+			b'+' => {
+				let (term, after_term) = parse_term_2(after_op)?;
+				expr = Expr::Sum(Box::new((expr, term)));
+				rest = after_term;
+			}
+			_ => break,
+		}
+	}
+	Some((expr, rest))
+}
+
+fn parse_term_2(tokens: &[u8]) -> Option<(Expr, &[u8])> {
+	let (first, rest) = tokens.split_first()?;
+	match first {
+		b'0'..=b'9' => Some((Expr::Number((*first - b'0').into()), rest)),
+		b'(' => {
+			let (expr, rest) = parse_expr_2(rest)?;
+			let (close_paren, rest) = rest.split_first()?;
+			(*close_paren == b')').then_some((expr, rest))
+		}
+		_ => None,
+	}
+}
+
+pub fn part2() -> u64 {
+	read_lines("input/18.txt")
+		.map(|line| evaluate(&parse_2(&line)))
+		.sum()
 }
