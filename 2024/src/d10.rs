fde633550ce9ed385ecb212aff3cbeb5477e120e
Jonathan Sharman <jonathan.sharman@niche.com>
2024-12-10 00:42:53 -0500

    2024-10-2

--- 2024/src/d10.rs
+++ 2024/src/d10.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use aoc::{
 	graph::Digraph,
 	grid::{Grid, Point},
@@ -6,20 +8,44 @@ use aoc::{
 };
 
 aoc::test::test_part!(test1, part1, 688);
-aoc::test::test_part!(test2, part2, ?);
+aoc::test::test_part!(test2, part2, 1459);
 
-pub fn part1() -> usize {
+struct Map {
+	trails: Digraph<Point>,
+	trailheads: HashSet<Point>,
+	peaks: HashSet<Point>,
+}
+
+impl Map {
+	fn rating(&self, start: Point) -> usize {
+		if self.peaks.contains(&start) {
+			return 1;
+		}
+		match self.trails.edges_from(&start) {
+			Some(edges) => {
+				edges.keys().map(|&neighbor| self.rating(neighbor)).sum()
+			}
+			None => 0,
+		}
+	}
+}
+
+fn read_map() -> Map {
 	let map = Grid::from_iter(
 		read_lines("input/10.txt")
 			.map(|line| line.bytes().map(|b| b - b'0').collect()),
 	);
 	let mut trails = Digraph::new();
-	let mut trailheads = Vec::new();
-	let mut peaks = Vec::new();
+	let mut trailheads = HashSet::new();
+	let mut peaks = HashSet::new();
 	for (node, height) in map.tiles() {
 		match height {
-			0 => trailheads.push(node),
-			9 => peaks.push(node),
+			0 => {
+				trailheads.insert(node);
+			}
+			9 => {
+				peaks.insert(node);
+			}
 			_ => {}
 		}
 		// TODO: Port neighbors functionality to the grid module.
@@ -38,13 +64,22 @@ pub fn part1() -> usize {
 			trails.insert_edge(node, neighbor, 1);
 		}
 	}
-	trailheads
+	Map {
+		trails,
+		trailheads,
+		peaks,
+	}
+}
+
+pub fn part1() -> usize {
+	let map = read_map();
+	map.trailheads
 		.into_iter()
 		.map(|trailhead| {
-			peaks
+			map.peaks
 				.iter()
 				.filter(|&peak| {
-					trails
+					map.trails
 						.shortest_distance(trailhead, |node| node == peak)
 						.is_some()
 				})
@@ -54,5 +89,9 @@ pub fn part1() -> usize {
 }
 
 pub fn part2() -> usize {
-	0
+	let map = read_map();
+	map.trailheads
+		.iter()
+		.map(|&trailhead| map.rating(trailhead))
+		.sum()
 }
