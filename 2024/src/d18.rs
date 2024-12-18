5acc401affddf858f23d46b4d7778fe0090bd655
Jonathan Sharman <jonathan.sharman@niche.com>
2024-12-18 00:42:32 -0500

    2024-18-2 - slow (4+ seconds)

--- 2024/src/d18.rs
+++ 2024/src/d18.rs
@@ -5,7 +5,7 @@ use aoc::{
 };
 
 aoc::test::test_part!(test1, part1, 432);
-aoc::test::test_part!(test2, part2, ?);
+aoc::test::test_part!(test2, part2, "56,27");
 
 const GRID_SIZE: Vector = Vector::new(71, 71);
 const BYTE_COUNT: usize = 1024;
@@ -27,6 +27,22 @@ pub fn part1() -> usize {
 		.unwrap()
 }
 
-pub fn part2() -> usize {
-	0
+pub fn part2() -> String {
+	let mut grid = Grid::new(GRID_SIZE, true);
+	let point = read_lines("input/18.txt")
+		.map(|line| {
+			let (col, row) = line.split_once(',').unwrap();
+			Point::new(row.parse().unwrap(), col.parse().unwrap())
+		})
+		.find(|&point| {
+			grid[point] = false;
+			let graph = graph::from_bool_grid(&grid);
+			graph
+				.shortest_distance(Point::zero(), |&p| {
+					p == Point::new(grid.row_count() - 1, grid.col_count() - 1)
+				})
+				.is_none()
+		})
+		.unwrap();
+	format!("{},{}", point.col, point.row)
 }
