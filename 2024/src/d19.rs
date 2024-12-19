de06d1fc4848566cca0e90da27cf88256f4c24bc
Jonathan Sharman <jonathan.sharman@niche.com>
2024-12-19 10:16:11 -0500

    2024-19-2

--- 2024/src/d19.rs
+++ 2024/src/d19.rs
@@ -1,9 +1,11 @@
+use std::collections::HashMap;
+
 use aoc::io::read_lines;
 use itertools::Itertools;
 use regex::Regex;
 
 aoc::test::test_part!(test1, part1, 340);
-aoc::test::test_part!(test2, part2, ?);
+aoc::test::test_part!(test2, part2, 717561822679428);
 
 pub fn part1() -> usize {
 	let mut lines = read_lines("input/19.txt");
@@ -12,6 +14,36 @@ pub fn part1() -> usize {
 	lines.skip(1).filter(|towel| regex.is_match(towel)).count()
 }
 
+fn combos<'a>(
+	patterns: &[String],
+	towel: &'a str,
+	cache: &mut HashMap<&'a str, usize>,
+) -> usize {
+	if let Some(&n) = cache.get(towel) {
+		return n;
+	}
+	let n = patterns
+		.iter()
+		.filter_map(|pattern| {
+			towel
+				.strip_prefix(pattern)
+				.map(|rest| combos(patterns, rest, cache))
+		})
+		.sum();
+	cache.insert(towel, n);
+	n
+}
+
 pub fn part2() -> usize {
-	0
+	let mut lines = read_lines("input/19.txt");
+	let patterns: Vec<String> = lines
+		.next()
+		.unwrap()
+		.split(", ")
+		.map(|pattern| pattern.to_string())
+		.collect();
+	lines
+		.skip(1)
+		.map(|towel| combos(&patterns, &towel, &mut HashMap::from([("", 1)])))
+		.sum()
 }
