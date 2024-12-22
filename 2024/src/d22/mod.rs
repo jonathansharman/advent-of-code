bc05d219ff4363f140892d1165909815c1f75fda
Jonathan Sharman <jonathan.sharman@niche.com>
2024-12-22 10:25:25 -0500

    2024-22-1

--- 2024/src/d22/mod.rs
+++ 2024/src/d22/mod.rs
@@ -1,10 +1,25 @@
-aoc::test::test_part!(test1, part1, ?);
+use aoc::input::{input, ParseLines};
+
+aoc::test::test_part!(test1, part1, 14180628689);
 aoc::test::test_part!(test2, part2, ?);
 
-const INPUT: &str = include_str!("input.txt");
+const MODULUS: usize = 16777216;
 
 pub fn part1() -> usize {
-	INPUT.lines().count()
+	input!()
+		.parse_lines()
+		.map(|mut secret: usize| {
+			for _ in 0..2000 {
+				secret ^= 64 * secret;
+				secret %= MODULUS;
+				secret ^= secret / 32;
+				secret %= MODULUS;
+				secret ^= 2048 * secret;
+				secret %= MODULUS;
+			}
+			secret
+		})
+		.sum()
 }
 
 pub fn part2() -> usize {
