e10500fdd4d9982ee0c4c77f1d29805696714503
Jonathan Sharman <jonathan.sharman@niche.com>
2024-12-24 12:24:56 -0500

    2024-23-2

--- 2024/src/d23/mod.rs
+++ 2024/src/d23/mod.rs
@@ -1,16 +1,24 @@
+use std::collections::HashSet;
+
 use aoc::{graph::Graph, input};
+use itertools::Itertools;
 
 aoc::test::test_part!(test1, part1, 1173);
 aoc::test::test_part!(test2, part2, ?);
 
-pub fn part1() -> usize {
+fn parse_graph() -> Graph<&'static str> {
 	let mut graph = Graph::new();
 	for line in input!().lines() {
 		let (left, right) = line.split_once('-').unwrap();
 		graph.insert_edge(left, right, 1);
 	}
+	graph
+}
+
+pub fn part1() -> usize {
+	let graph = parse_graph();
 	let mut sets = 0;
-	let nodes: Vec<_> = graph.nodes().iter().collect();
+	let nodes: Vec<_> = graph.nodes().iter().copied().collect();
 	for (i, &n1) in nodes.iter().enumerate() {
 		for (j, &n2) in nodes.iter().enumerate().skip(i) {
 			for &n3 in nodes.iter().skip(j) {
@@ -29,6 +37,26 @@ pub fn part1() -> usize {
 	sets
 }
 
-pub fn part2() -> usize {
-	0
+pub fn part2() -> String {
+	let graph = parse_graph();
+	let mut cliques = Vec::from([HashSet::new()]);
+	'nodes: for node in graph.nodes() {
+		'cliques: for clique in cliques.iter_mut() {
+			for &other in clique.iter() {
+				if graph.weight(node, other).is_none() {
+					continue 'cliques;
+				}
+			}
+			clique.insert(node);
+			continue 'nodes;
+		}
+		cliques.push(HashSet::from([node]));
+	}
+	cliques
+		.into_iter()
+		.max_by_key(|clique| clique.len())
+		.unwrap()
+		.into_iter()
+		.sorted()
+		.join(",")
 }
