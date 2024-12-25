0abce9db5757cf250f6420a599f1583ec1736aef
Jonathan Sharman <jonathan.sharman@niche.com>
2024-12-25 08:30:41 -0500

    2024-25

--- 2024/src/d25/mod.rs
+++ 2024/src/d25/mod.rs
@@ -1,7 +1,41 @@
-use aoc::input;
+use aoc::{grid::Grid, input};
+use itertools::Itertools;
 
-aoc::test::test_part!(test1, part1, ?);
+aoc::test::test_part!(test1, part1, 3320);
 
 pub fn part1() -> usize {
-	input!().lines().count()
+	let mut locks = Vec::new();
+	let mut keys = Vec::new();
+
+	let mut lines = input!().lines();
+	loop {
+		let grid: Grid<char> = lines
+			.by_ref()
+			.take(7)
+			.map(|line| line.chars().collect())
+			.collect();
+		let item: Vec<usize> = grid
+			.cols()
+			.map(|col| col.filter(|&&c| c == '#').count())
+			.collect();
+		if grid.get_row(0).unwrap().copied().eq("#####".chars()) {
+			locks.push(item);
+		} else {
+			keys.push(item);
+		}
+
+		if lines.next().is_none() {
+			break;
+		}
+	}
+
+	locks
+		.iter()
+		.cartesian_product(keys.iter())
+		.filter(|(lock, key)| {
+			lock.iter()
+				.zip(key.iter())
+				.all(|(pin, tooth)| pin + tooth <= 7)
+		})
+		.count()
 }
