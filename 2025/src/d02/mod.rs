4754d02b722555c4a20b7b5e32576d6b3a66b8a2
Jonathan Sharman <jonathan.sharman@gmail.com>
2025-12-02 01:02:59 -0500

    2025-02-2 (brute force)

--- 2025/src/d02/mod.rs
+++ 2025/src/d02/mod.rs
@@ -1,8 +1,8 @@
 use aoc::input;
 use itertools::Itertools;
 
-aoc::test::test_part!(test1, part1, ?);
-aoc::test::test_part!(test2, part2, ?);
+aoc::test::test_part!(test1, part1, 32976912643);
+aoc::test::test_part!(test2, part2, 54446379122);
 
 fn ranges() -> impl Iterator<Item = (usize, usize)> {
 	input!().split(',').map(|range| {
@@ -26,5 +26,28 @@ pub fn part1() -> usize {
 }
 
 pub fn part2() -> usize {
-	0
+	ranges()
+		.flat_map(|(min, max)| {
+			(min..=max).filter(|id| {
+				let id = id.to_string();
+				'chunks: for chunk_size in 1..=id.len() / 2 {
+					let chunks: Vec<String> = id
+						.chars()
+						.chunks(chunk_size)
+						.into_iter()
+						.map(|chunk| chunk.collect())
+						.collect();
+					for (i, chunk1) in chunks.iter().enumerate() {
+						for chunk2 in &chunks[i..] {
+							if chunk1 != chunk2 {
+								continue 'chunks;
+							}
+						}
+					}
+					return true;
+				}
+				false
+			})
+		})
+		.sum()
 }
