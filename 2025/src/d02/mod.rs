42ddc08cf6f39ff4a2fa1d1113f4d4e5fa9a1e33
Jonathan Sharman <jonathan.sharman@gmail.com>
2025-12-02 00:46:38 -0500

    2025-02-1

--- 2025/src/d02/mod.rs
+++ 2025/src/d02/mod.rs
@@ -1,10 +1,28 @@
 use aoc::input;
+use itertools::Itertools;
 
 aoc::test::test_part!(test1, part1, ?);
 aoc::test::test_part!(test2, part2, ?);
 
+fn ranges() -> impl Iterator<Item = (usize, usize)> {
+	input!().split(',').map(|range| {
+		range
+			.split('-')
+			.map(|s| s.parse::<usize>().unwrap())
+			.collect_tuple()
+			.unwrap()
+	})
+}
+
 pub fn part1() -> usize {
-	input!().lines().count()
+	ranges()
+		.flat_map(|(min, max)| {
+			(min..=max).filter(|id| {
+				let id = id.to_string();
+				id[..id.len() / 2] == id[id.len() / 2..]
+			})
+		})
+		.sum()
 }
 
 pub fn part2() -> usize {
