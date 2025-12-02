8b3d42ca7cdbaeefd092a7e4607661ae440c82f4
Jonathan Sharman <jonathan.sharman@gmail.com>
2025-12-01 23:54:33 -0500

    2025-01-1

--- 2025/src/d01/mod.rs
+++ 2025/src/d01/mod.rs
@@ -1,10 +1,30 @@
 use aoc::input;
 
-aoc::test::test_part!(test1, part1, ?);
+aoc::test::test_part!(test1, part1, 1055);
 aoc::test::test_part!(test2, part2, ?);
 
+fn rotations() -> impl Iterator<Item = i32> {
+	input!().lines().map(|s| {
+		let bytes = s.as_bytes();
+		let value =
+			str::from_utf8(&bytes[1..]).unwrap().parse::<i32>().unwrap();
+		if bytes[0] == b'L' { -value } else { value }
+	})
+}
+
+const MOD: i32 = 100;
+
 pub fn part1() -> usize {
-	input!().lines().count()
+	let mut dial = 50;
+	let mut zeros = 0;
+	for rotation in rotations() {
+		dial += rotation;
+		dial = (dial % MOD + MOD) % MOD;
+		if dial == 0 {
+			zeros += 1;
+		}
+	}
+	zeros
 }
 
 pub fn part2() -> usize {
