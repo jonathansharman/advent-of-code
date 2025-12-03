390695c17ddef11edfdb3e1d49027f7e3343e33b
Jonathan Sharman <jonathan.sharman@gmail.com>
2025-12-03 00:28:07 -0500

    2025-03-1

--- 2025/src/d03/mod.rs
+++ 2025/src/d03/mod.rs
@@ -1,10 +1,27 @@
 use aoc::input;
 
-aoc::test::test_part!(test1, part1, ?);
+aoc::test::test_part!(test1, part1, 17408);
 aoc::test::test_part!(test2, part2, ?);
 
 pub fn part1() -> usize {
-	input!().lines().count()
+	input!()
+		.lines()
+		.map(|bank| {
+			let batteries = bank.as_bytes();
+			batteries[..batteries.len() - 1]
+				.iter()
+				.enumerate()
+				.map(|(i, b1)| {
+					batteries[i + 1..]
+						.iter()
+						.map(|b2| (10 * (b1 - b'0') + b2 - b'0') as usize)
+						.max()
+						.unwrap()
+				})
+				.max()
+				.unwrap()
+		})
+		.sum()
 }
 
 pub fn part2() -> usize {
