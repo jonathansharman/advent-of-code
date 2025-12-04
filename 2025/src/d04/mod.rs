8ed825130b4a4222de3dc9a172e650123accab84
Jonathan Sharman <jonathan.sharman@gmail.com>
2025-12-04 00:10:28 -0500

    2025-04-1

--- 2025/src/d04/mod.rs
+++ 2025/src/d04/mod.rs
@@ -1,10 +1,16 @@
-use aoc::input;
+use aoc::{input, input::ParseGrid};
 
-aoc::test::test_part!(test1, part1, ?);
+aoc::test::test_part!(test1, part1, 1569);
 aoc::test::test_part!(test2, part2, ?);
 
 pub fn part1() -> usize {
-	input!().lines().count()
+	let forklifts = input!().parse_grid(|c| c == '@');
+	forklifts
+		.iter()
+		.filter(|&(p, &f)| {
+			f && forklifts.eight_neighbors(p).filter(|&(_, &n)| n).count() < 4
+		})
+		.count()
 }
 
 pub fn part2() -> usize {
