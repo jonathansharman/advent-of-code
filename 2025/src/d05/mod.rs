7b9831e139a047e24002304475ae989528d1120d
Jonathan Sharman <jonathan.sharman@gmail.com>
2025-12-05 00:28:15 -0500

    2025-05-1

--- 2025/src/d05/mod.rs
+++ 2025/src/d05/mod.rs
@@ -1,10 +1,33 @@
-use aoc::input;
+use std::ops::RangeInclusive;
 
-aoc::test::test_part!(test1, part1, ?);
+use aoc::input;
+use itertools::Itertools;
+
+aoc::test::test_part!(test1, part1, 643);
 aoc::test::test_part!(test2, part2, ?);
 
 pub fn part1() -> usize {
-	input!().lines().count()
+	let mut lines = input!().lines();
+	let fresh_ranges: Vec<RangeInclusive<usize>> = lines
+		.by_ref()
+		.map_while(|line| {
+			(!line.is_empty()).then(|| {
+				let (a, b) = line
+					.split('-')
+					.map(|n| n.parse().unwrap())
+					.collect_tuple()
+					.unwrap();
+				a..=b
+			})
+		})
+		.collect();
+	lines
+		.filter(|&ingredient| {
+			fresh_ranges
+				.iter()
+				.any(|range| range.contains(&ingredient.parse().unwrap()))
+		})
+		.count()
 }
 
 pub fn part2() -> usize {
