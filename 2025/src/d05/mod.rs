4eb25249a60dc44bdf9011f501c32cde5b1d7fd2
Jonathan Sharman <jonathan.sharman@gmail.com>
2025-12-05 01:11:47 -0500

    2025-05-2

--- 2025/src/d05/mod.rs
+++ 2025/src/d05/mod.rs
@@ -4,11 +4,12 @@ use aoc::input;
 use itertools::Itertools;
 
 aoc::test::test_part!(test1, part1, 643);
-aoc::test::test_part!(test2, part2, ?);
+aoc::test::test_part!(test2, part2, 342018167474526);
 
-pub fn part1() -> usize {
-	let mut lines = input!().lines();
-	let fresh_ranges: Vec<RangeInclusive<usize>> = lines
+type Range = RangeInclusive<usize>;
+
+fn get_ranges(lines: &mut impl Iterator<Item = &'static str>) -> Vec<Range> {
+	lines
 		.by_ref()
 		.map_while(|line| {
 			(!line.is_empty()).then(|| {
@@ -20,16 +21,49 @@ pub fn part1() -> usize {
 				a..=b
 			})
 		})
-		.collect();
+		.collect()
+}
+
+pub fn part1() -> usize {
+	let mut lines = input!().lines();
+	let ranges = get_ranges(lines.by_ref());
 	lines
 		.filter(|&ingredient| {
-			fresh_ranges
+			ranges
 				.iter()
 				.any(|range| range.contains(&ingredient.parse().unwrap()))
 		})
 		.count()
 }
 
+/// The overlapping range of `r1` and `r2` or else `None`.
+fn merge_ranges(r1: &Range, r2: &Range) -> Option<Range> {
+	// Ensure the lower bound of r1 is not greater than that of r2.
+	let (r1, r2) = if r1.start() > r2.start() {
+		(r2, r1)
+	} else {
+		(r1, r2)
+	};
+
+	(*r2.start() <= r1.end() + 1)
+		.then_some(*r1.start()..=*(r1.end().max(r2.end())))
+}
+
 pub fn part2() -> usize {
-	0
+	let mut merged = Vec::new();
+	let mut queue = get_ranges(&mut input!().lines());
+	'queue: while let Some(next) = queue.pop() {
+		for i in 0..merged.len() {
+			if let Some(new) = merge_ranges(&merged[i], &next) {
+				merged.swap_remove(i);
+				queue.push(new);
+				continue 'queue;
+			}
+		}
+		merged.push(next);
+	}
+	merged
+		.into_iter()
+		.map(|range| range.end() - range.start() + 1)
+		.sum()
 }
