84dcee7fab8191400b5bd8c1cbb967b37c275087
Jonathan Sharman <jonathan.sharman@gmail.com>
2025-12-07 17:45:26 -0500

    2025-07-2

--- 2025/src/d07/mod.rs
+++ 2025/src/d07/mod.rs
@@ -1,13 +1,14 @@
-use std::fmt::Debug;
+use std::{collections::HashMap, fmt::Debug};
 
 use aoc::{
-	grid::{Point, Vector},
+	grid::{Grid, Point, Vector},
 	input,
 	input::ParseGrid,
 };
+use itertools::Itertools;
 
 aoc::test::test_part!(test1, part1, 1717);
-aoc::test::test_part!(test2, part2, ?);
+aoc::test::test_part!(test2, part2, 231507396180012);
 
 #[derive(PartialEq, Eq, Clone, Copy)]
 enum Tile {
@@ -26,13 +27,12 @@ impl Debug for Tile {
 	}
 }
 
-pub fn part1() -> usize {
+fn split_beams() -> Grid<Tile> {
 	let mut diagram = input!().parse_grid(|c| match c {
 		'S' => Tile::Beam,
 		'^' => Tile::Splitter,
 		_ => Tile::Space,
 	});
-	let mut splits = 0;
 	for i in 1..diagram.row_count() {
 		for j in 0..diagram.col_count() {
 			if diagram[(i - 1, j).into()] != Tile::Beam {
@@ -40,7 +40,6 @@ pub fn part1() -> usize {
 			}
 			let coords = Point::new(i, j);
 			if let Tile::Splitter = diagram[coords] {
-				splits += 1;
 				diagram[coords - Vector::new(0, 1)] = Tile::Beam;
 				diagram[coords + Vector::new(0, 1)] = Tile::Beam;
 			} else {
@@ -48,9 +47,51 @@ pub fn part1() -> usize {
 			}
 		}
 	}
-	splits
+	diagram
+}
+
+pub fn part1() -> usize {
+	let diagram = split_beams();
+	diagram
+		.cols()
+		.map(|col| {
+			col.tuple_windows()
+				.filter(|&(&top, &bottom)| {
+					top == Tile::Beam && bottom == Tile::Splitter
+				})
+				.count()
+		})
+		.sum()
+}
+
+fn paths(
+	cache: &mut HashMap<Point, usize>,
+	diagram: &Grid<Tile>,
+	start: Point,
+) -> usize {
+	if let Some(&paths) = cache.get(&start) {
+		return paths;
+	}
+
+	let paths = match diagram.get(start) {
+		Some(Tile::Space) => panic!("where did the beam go?"),
+		Some(Tile::Splitter) => {
+			paths(cache, diagram, start + Vector::new(1, -1))
+				+ paths(cache, diagram, start + Vector::new(1, 1))
+		}
+		Some(Tile::Beam) => paths(cache, diagram, start + Vector::new(1, 0)),
+		None => 1,
+	};
+	cache.insert(start, paths);
+	paths
 }
 
 pub fn part2() -> usize {
-	0
+	let diagram = split_beams();
+	let start = diagram
+		.get_row(0)
+		.unwrap()
+		.position(|&tile| tile == Tile::Beam)
+		.unwrap() as i64;
+	paths(&mut HashMap::new(), &diagram, Point::new(0, start))
 }
