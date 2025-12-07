bf6ad532503d7d87b31a00963d8e5eaf18272335
Jonathan Sharman <jonathan.sharman@gmail.com>
2025-12-07 00:46:51 -0500

    2025-07-1

--- 2025/src/d07/mod.rs
+++ 2025/src/d07/mod.rs
@@ -1,10 +1,54 @@
-use aoc::input;
+use std::fmt::Debug;
 
-aoc::test::test_part!(test1, part1, ?);
+use aoc::{
+	grid::{Point, Vector},
+	input,
+	input::ParseGrid,
+};
+
+aoc::test::test_part!(test1, part1, 1717);
 aoc::test::test_part!(test2, part2, ?);
 
+#[derive(PartialEq, Eq, Clone, Copy)]
+enum Tile {
+	Space,
+	Splitter,
+	Beam,
+}
+
+impl Debug for Tile {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		match self {
+			Self::Space => write!(f, "."),
+			Self::Splitter => write!(f, "^"),
+			Self::Beam => write!(f, "|"),
+		}
+	}
+}
+
 pub fn part1() -> usize {
-	input!().lines().count()
+	let mut diagram = input!().parse_grid(|c| match c {
+		'S' => Tile::Beam,
+		'^' => Tile::Splitter,
+		_ => Tile::Space,
+	});
+	let mut splits = 0;
+	for i in 1..diagram.row_count() {
+		for j in 0..diagram.col_count() {
+			if diagram[(i - 1, j).into()] != Tile::Beam {
+				continue;
+			}
+			let coords = Point::new(i, j);
+			if let Tile::Splitter = diagram[coords] {
+				splits += 1;
+				diagram[coords - Vector::new(0, 1)] = Tile::Beam;
+				diagram[coords + Vector::new(0, 1)] = Tile::Beam;
+			} else {
+				diagram[coords] = Tile::Beam;
+			}
+		}
+	}
+	splits
 }
 
 pub fn part2() -> usize {
