ff6a86905101f0fdfd2767104040dafcdf2beb90
Jonathan Sharman <jonathan.sharman@gmail.com>
2025-12-09 00:24:43 -0500

    2025-09-1

--- 2025/src/d09/mod.rs
+++ 2025/src/d09/mod.rs
@@ -1,10 +1,25 @@
-use aoc::input;
+use aoc::{grid::Point, input, input::ParseCommaSeparated};
+use itertools::Itertools;
 
-aoc::test::test_part!(test1, part1, ?);
+aoc::test::test_part!(test1, part1, 4765757080);
 aoc::test::test_part!(test2, part2, ?);
 
-pub fn part1() -> usize {
-	input!().lines().count()
+pub fn part1() -> u64 {
+	let reds: Vec<Point> = input!()
+		.lines()
+		.map(|line| {
+			let (row, col) =
+				line.parse_comma_separated().collect_tuple().unwrap();
+			Point::new(row, col)
+		})
+		.collect();
+	reds.iter()
+		.cartesian_product(reds.iter())
+		.map(|(p1, p2)| {
+			(p1.row.abs_diff(p2.row) + 1) * (p1.col.abs_diff(p2.col) + 1)
+		})
+		.max()
+		.unwrap()
 }
 
 pub fn part2() -> usize {
