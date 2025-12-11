7a81e6adc71ff626ae9ab0bb554284f5d11d54e4
Jonathan Sharman <jonathan.sharman@gmail.com>
2025-12-11 00:33:32 -0500

    2025-11-2

--- 2025/src/d11/mod.rs
+++ 2025/src/d11/mod.rs
@@ -1,7 +1,19 @@
+use std::collections::HashMap;
+
 use aoc::{graph::Digraph, input};
 
 aoc::test::test_part!(test1, part1, 649);
 aoc::test::test_part!(test2, part2, ?);
+
+fn parse_network() -> Digraph<&'static str> {
+	input!()
+		.lines()
+		.flat_map(|line| {
+			let (source, sinks) = line.split_once(": ").unwrap();
+			sinks.split_whitespace().map(move |sink| (source, sink, 1))
+		})
+		.collect()
+}
 
 fn paths_to_out(
 	network: &Digraph<&'static str>,
@@ -19,16 +31,46 @@ fn paths_to_out(
 }
 
 pub fn part1() -> usize {
-	let network: Digraph<&'static str> = input!()
-		.lines()
-		.flat_map(|line| {
-			let (source, sinks) = line.split_once(": ").unwrap();
-			sinks.split_whitespace().map(move |sink| (source, sink, 1))
-		})
-		.collect();
+	let network = parse_network();
 	paths_to_out(&network, "you")
 }
 
+fn paths_through_dac_and_fft(
+	cache: &mut HashMap<(&'static str, bool, bool), usize>,
+	network: &Digraph<&'static str>,
+	source: &'static str,
+	mut dac: bool,
+	mut fft: bool,
+) -> usize {
+	if let Some(&paths) = cache.get(&(source, dac, fft)) {
+		return paths;
+	}
+
+	if source == "out" {
+		return if dac && fft { 1 } else { 0 };
+	}
+	dac |= source == "dac";
+	fft |= source == "fft";
+	let paths = match network.edges_from(source) {
+		Some(sinks) => sinks
+			.keys()
+			.map(|&sink| {
+				paths_through_dac_and_fft(cache, network, sink, dac, fft)
+			})
+			.sum(),
+		None => 0,
+	};
+	cache.insert((source, dac, fft), paths);
+	paths
+}
+
 pub fn part2() -> usize {
-	0
+	let network = parse_network();
+	paths_through_dac_and_fft(
+		&mut HashMap::new(),
+		&network,
+		"svr",
+		false,
+		false,
+	)
 }
